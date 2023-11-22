d0f0848bcc0b0ab862939ddc1a6c488ca5515853
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-11-22 19:28:09 +0200

    Create 1_reverse_numbers..py

---
+++ Lists as Stacks and Queues - Exercise/1_reverse_numbers..py
@@ -0,0 +1,9 @@
+numbers = list(map(int, input().split()))
+
+while len(numbers) > 0:
+    element = numbers.pop()
+    print(element, end=' ')
+
+
+# numbers = list(map(int, input().split()))
+# print(' '.join(map(str, numbers[::-1])))
