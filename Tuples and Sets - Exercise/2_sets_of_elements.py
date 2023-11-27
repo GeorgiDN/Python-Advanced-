1775d3c54392a71aa8de37d035b4dbd0e0d19d89
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-11-27 19:33:53 +0200

    Update 2_sets_of_elements.py

--- Tuples and Sets - Exercise/2_sets_of_elements.py
+++ Tuples and Sets - Exercise/2_sets_of_elements.py
@@ -23,3 +23,28 @@ for num in list_1:
 for curr_num in repeat:
     print(''.join(str(curr_num)))
   
+
+
+
+# def input_lines(num):
+#     lines = set()
+#     for _ in range(num):
+#         lines.add(input())
+#     return lines
+#
+#
+# def intersection(a, b):
+#     return a.intersection(b)
+#
+#
+# def print_result(res):
+#     for r in res:
+#         print(r)
+#
+#
+# len1, len2 = input().split(" ")
+#
+# line1 = input_lines(int(len1))
+# line2 = input_lines(int(len2))
+#
+# print_result(intersection(line1, line2))
