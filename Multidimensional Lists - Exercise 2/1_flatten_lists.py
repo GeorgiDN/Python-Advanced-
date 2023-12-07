e9297cbdddc7aa2f3d3ed8c59989e58fc301796f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-07 19:33:49 +0200

    Update 1_flatten_lists.py

--- Multidimensional Lists - Exercise 2/1_flatten_lists.py
+++ Multidimensional Lists - Exercise 2/1_flatten_lists.py
@@ -12,3 +12,13 @@ while joined_lists:
     current_list = joined_lists.pop()
     flattened_list.extend(current_list)
 print(*flattened_list)
+
+
+
+# joined_lists = [[x.strip() for x in sublist.split()] for sublist in input().split("|")]
+# flattened_list = []
+# while joined_lists:
+#     current_list = joined_lists.pop()
+#     flattened_list.extend(current_list)
+# print(*flattened_list)
+
