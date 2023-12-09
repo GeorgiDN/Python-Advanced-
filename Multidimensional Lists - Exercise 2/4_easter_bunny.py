800a200f672d5515c959d0adc7e2f03b9c4c0669
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-09 19:01:17 +0200

    Update 4_easter_bunny.py

--- Multidimensional Lists - Exercise 2/4_easter_bunny.py
+++ Multidimensional Lists - Exercise 2/4_easter_bunny.py
@@ -55,3 +55,67 @@ direction, positions, collected_eggs = best_path(bunny_row, bunny_col)
 print(direction)
 [print(position) for position in positions]
 print(collected_eggs)
+
+
+
+
+#
+# def collect_eggs(row, col, path):
+#     sum_eggs = 0
+#     bunny_positions = []
+#     if path == "up":
+#         for idx in range(row - 1, -1, -1):
+#             current_egg = matrix[idx][col]
+#             if current_egg == 'X':
+#                 break
+#             sum_eggs += int(current_egg)
+#             bunny_positions.append([idx, col])
+#     elif path == "down":
+#         for idx in range(row + 1, size):
+#             current_egg = matrix[idx][col]
+#             if current_egg == 'X':
+#                 break
+#             sum_eggs += int(current_egg)
+#             bunny_positions.append([idx, col])
+#     elif path == "left":
+#         for idx in range(col - 1, -1, -1):
+#             current_egg = matrix[row][idx]
+#             if current_egg == 'X':
+#                 break
+#             sum_eggs += int(current_egg)
+#             bunny_positions.append([row, idx])
+#     elif path == "right":
+#         for idx in range(col + 1, size):
+#             current_egg = matrix[row][idx]
+#             if current_egg == 'X':
+#                 break
+#             sum_eggs += int(current_egg)
+#             bunny_positions.append([row, idx])
+
+#     return sum_eggs, bunny_positions
+
+
+# size = int(input())
+# matrix = []
+
+# for _ in range(size):
+#     matrix.append([x for x in input().split()])
+
+# max_sum_eggs = 0
+# direction = ''
+# max_sum_moves = []
+
+# for row in range(size):
+#     for col in range(size):
+#         if matrix[row][col] == "B":
+#             for path in ["up", "down", "left", "right"]:
+#                 sum_eggs, bunny_positions = collect_eggs(row, col, path)
+#                 if sum_eggs > max_sum_eggs:
+#                     max_sum_eggs = sum_eggs
+#                     direction = path
+#                     max_sum_moves = bunny_positions
+
+# print(direction)
+# for points in max_sum_moves:
+#     print(points)
+# print(max_sum_eggs)
