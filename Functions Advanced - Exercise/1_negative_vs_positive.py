3a64c1bf901287c0e0d5f5c7002223cc7e0cd815
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-13 19:20:08 +0200

    Update 1_negative_vs_positive.py

--- Functions Advanced - Exercise/1_negative_vs_positive.py
+++ Functions Advanced - Exercise/1_negative_vs_positive.py
@@ -1,22 +1,46 @@
-def negative_vs_positive(nums):
-    negative_sum = []
-    positive_sum = []
-    for num in nums:
-        if num < 0:
-            negative_sum.append(num)
-        elif num > 0:
-            positive_sum.append(num)
-
-    total_negative = (sum(negative_sum))
-    total_positive = (sum(positive_sum))
-
-    print(total_negative)
-    print(total_positive)
-
-    if abs(total_negative) > total_positive:
-        print("The negatives are stronger than the positives")
-    elif abs(total_negative) < total_positive:
-        print("The positives are stronger than the negatives")
+def sort_numbers():
+    [negatives.append(x) for x in numbers if x < 0]
+    [positives.append(x) for x in numbers if x >= 0]
 
 
-numbers = negative_vs_positive(list(map(int, input().split(" "))))
+numbers = [int(x) for x in input().split()]
+negatives, positives = [], []
+
+sort_numbers()
+
+print(sum(negatives))
+print(sum(positives))
+
+if abs(sum(negatives)) > sum(positives):
+    print("The negatives are stronger than the positives")
+else:
+    print("The positives are stronger than the negatives")
+
+
+
+
+# def negative_vs_positive(nums):
+#     negative_sum = []
+#     positive_sum = []
+#     for num in nums:
+#         if num < 0:
+#             negative_sum.append(num)
+#         elif num > 0:
+#             positive_sum.append(num)
+
+#     total_negative = (sum(negative_sum))
+#     total_positive = (sum(positive_sum))
+
+#     print(total_negative)
+#     print(total_positive)
+
+#     if abs(total_negative) > total_positive:
+#         print("The negatives are stronger than the positives")
+#     elif abs(total_negative) < total_positive:
+#         print("The positives are stronger than the negatives")
+
+
+# numbers = negative_vs_positive(list(map(int, input().split(" "))))
+
+
+
