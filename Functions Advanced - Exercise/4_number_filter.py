672e9db2fa37a30598c333e995d6c2cfc3183acd
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-14 18:51:56 +0200

    Create 4_number_filter.py

---
+++ Functions Advanced - Exercise/4_number_filter.py
@@ -0,0 +1,22 @@
+def even_odd_filter(**kwargs):
+    result = {}
+
+    for key in kwargs:
+        if key == "odd":
+            result[key] = [x for x in kwargs[key] if x % 2 != 0]
+        elif key == "even":
+            result[key] = [x for x in kwargs[key] if x % 2 == 0]
+
+    return dict(sorted(result.items(), key=lambda x: -len(x[1])))
+
+
+
+# print(even_odd_filter(
+#     odd=[2, 2, 30, 44, 10, 5],
+# ))
+
+print(even_odd_filter(
+    odd=[1, 2, 3, 4, 10, 5],
+    even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+))
+
