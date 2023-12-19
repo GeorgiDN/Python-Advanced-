c9965e45179894334db02b1d612b5cee2448b861
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-19 18:58:43 +0200

    Update 2_file_reader.py

--- File Handling-Lab/2_file_reader.py
+++ File Handling-Lab/2_file_reader.py
@@ -1,6 +1,15 @@
-numbers_file = open('numbers.txt', 'r')
-numbers_sum = 0
-for number in numbers_file:
-    numbers_sum += int(number)
-print(numbers_sum)
-numbers_file.close()
+sum_numbers = 0
+
+with open("numbers.txt") as file:
+    for line in file:
+        sum_numbers += (int(line))
+
+print(sum_numbers)
+
+
+# numbers_file = open('numbers.txt', 'r')
+# numbers_sum = 0
+# for number in numbers_file:
+#     numbers_sum += int(number)
+# print(numbers_sum)
+# numbers_file.close()
