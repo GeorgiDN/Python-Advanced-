a18667e4298ba0d93ab92be5a3684f9ff23e2170
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-19 18:58:15 +0200

    Update 1_file_opener.py

--- File Handling-Lab/1_file_opener.py
+++ File Handling-Lab/1_file_opener.py
@@ -1,8 +1,21 @@
 file_name = "text.txt"
 
 try:
-    file = open(file_name, "r")
-    print("File found")
-    file.close()
+    with open(file_name, "r") as file:
+        file_content = file.read()
+        print("File found")
+
 except FileNotFoundError:
     print("File not found")
+
+
+
+
+# file_name = "text.txt"
+
+# try:
+#     file = open(file_name, "r")
+#     print("File found")
+#     file.close()
+# except FileNotFoundError:
+#     print("File not found")
