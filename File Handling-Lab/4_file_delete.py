64faffc6780b89a8cc8eedadc02e4f92f9fcfbc9
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-20 19:45:04 +0200

    Update 4_file_delete.py

--- File Handling-Lab/4_file_delete.py
+++ File Handling-Lab/4_file_delete.py
@@ -1,7 +1,18 @@
 import os
 
-file_path = "file_for_delete.txt"
-if os.path.exists(file_path):
-    os.remove(file_path)
-else:
-    print(f"There is no file found with name: {file_path}")
+try:
+    os.remove("file_for_delete.txt")
+except FileNotFoundError:
+    print("File already deleted or doesn't exist!")
+
+
+
+
+
+# import os
+
+# file_path = "file_for_delete.txt"
+# if os.path.exists(file_path):
+#     os.remove(file_path)
+# else:
+#     print(f"There is no file found with name: {file_path}")
