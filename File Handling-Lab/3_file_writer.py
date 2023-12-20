43604926bca8b946ab5298794e2c947420f1e34c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-20 19:44:43 +0200

    Update 3_file_writer.py

--- File Handling-Lab/3_file_writer.py
+++ File Handling-Lab/3_file_writer.py
@@ -1,7 +1,18 @@
-file = open("my_first_file.txt", "a")
-file.write("This is my first line.\n")
-more_information = ["Multiple line\n",
-                    "Adding the lines to a list\n",
-                    "But we have to use '.writelines()'\n"]
-file.writelines(more_information)
-file.close()
+with open("my_first_file.txt", "w") as f:
+    f.write('This is my first line.\n')
+    more_info = ["Multiple line\n",
+                 "Add line to a list\n"
+                 "And we use .writelines()\n"]
+    f.writelines(more_info)
+
+
+
+
+# file = open("my_first_file.txt", "a")
+# file.write("This is my first line.\n")
+# more_information = ["Multiple line\n",
+#                     "Adding the lines to a list\n",
+#                     "But we have to use '.writelines()'\n"]
+# file.writelines(more_information)
+# file.close()
+
