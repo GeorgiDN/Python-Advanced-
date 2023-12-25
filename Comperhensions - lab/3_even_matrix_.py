3d94f2eb502e691a231be1cf50ecc6790fbfbb0e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-25 20:05:28 +0200

    Update 3_even_matrix_.py

--- Comperhensions - lab/3_even_matrix_.py
+++ Comperhensions - lab/3_even_matrix_.py
@@ -2,3 +2,23 @@ n = int(input())
 matrix = [[int(x) for x in input().split(", ") if int(x) % 2 == 0] for _ in range(n)]
 print(
     matrix)
+
+
+# def read_matrix(r):
+#     matrix = [[int(x) for x in input().split(", ")] for _ in range(r)]
+#     return matrix
+
+
+# def get_even_row(row):
+#     result = [x for x in row if x % 2 == 0]
+#     return result
+
+
+# def get_even_matrix(matrix):
+#     result = [get_even_row(row) for row in matrix]
+#     return result
+
+
+# rows = int(input())
+
+# print(get_even_matrix(read_matrix(rows)))
