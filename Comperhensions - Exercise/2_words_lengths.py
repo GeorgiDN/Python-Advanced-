fdbee49e08a1a9ec97eeb0fa13e3af30e6af5763
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-26 19:16:26 +0200

    Update 2_words_lengths.py

--- Comperhensions - Exercise/2_words_lengths.py
+++ Comperhensions - Exercise/2_words_lengths.py
@@ -1,1 +1,21 @@
-print(", ".join([f"{n} -> {len(n)}" for n in input().split(", ")]))
+def word_length(word):
+    return len(word)
+
+
+def create_a_model(words):
+    return ", ".join([f"{n} -> {word_length(n)}" for n in words])
+
+
+def print_result(res):
+    return print(res)
+
+
+def take_input():
+    return input().split(", ")
+
+
+print_result(create_a_model(take_input()))
+
+
+# One row
+#print(", ".join([f"{n} -> {len(n)}" for n in input().split(", ")]))
