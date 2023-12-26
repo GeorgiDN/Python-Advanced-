9bfd6b1768ab403e6975206bff386ea512ed3fec
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-26 19:17:28 +0200

    Update 3_capitals.py

--- Comperhensions - Exercise/3_capitals.py
+++ Comperhensions - Exercise/3_capitals.py
@@ -1,1 +1,14 @@
-print("\n".join([f"{country} -> {capital}" for country, capital in zip(input().split(", "), input().split(", "))]))
+def get_capitals():
+    capitals = {country: city for country, city in zip(input().split(", "), input().split(", "))}
+    return capitals
+
+
+def print_result():
+    return [print(f"{country} -> {city}") for country, city in get_capitals().items()]
+
+
+print_result()
+
+
+# One row
+# print("\n".join([f"{country} -> {capital}" for country, capital in zip(input().split(", "), input().split(", "))]))
