ba7913efa1e10700fa0e84159a6a904c25d24da0
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-26 19:15:31 +0200

    Update 1_word_filter.py

--- Comperhensions - Exercise/1_word_filter.py
+++ Comperhensions - Exercise/1_word_filter.py
@@ -1,1 +1,16 @@
-print('\n'.join([product for product in input().split(" ") if len(product) % 2 == 0]))
+def word_filter(text):
+    result = [product for product in text if len(product) % 2 == 0]
+    return result
+
+
+def print_result(txt):
+    return [print(word) for word in txt]
+
+
+products = input().split(" ")
+print_result(word_filter(products))
+
+
+
+# One row
+#print('\n'.join([product for product in input().split(" ") if len(product) % 2 == 0]))
