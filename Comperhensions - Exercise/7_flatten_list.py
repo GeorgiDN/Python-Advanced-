b828f6b0d08cce3e881cbd4a6e2e97bf55ad80c7
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2023-12-28 16:35:19 +0200

    Update 7_flatten_list.py

--- Comperhensions - Exercise/7_flatten_list.py
+++ Comperhensions - Exercise/7_flatten_list.py
@@ -1,6 +1,33 @@
-nums = input().split("|")
-# joined_lists = [pair.strip() for pair in reversed(nums)]
-joined_lists = [pair for pair in reversed(nums)]
-flattened_list = [num for sublist in joined_lists for num in sublist.split()]
-result = list(map(int, flattened_list))
-print(*result)
+def take_input():
+    return input().split("|")
+
+
+def reversed_nums(nums):
+    return [pair for pair in reversed(nums)]
+
+
+def ordered_nums(rev_nums):
+    return [num for sublist in rev_nums for num in sublist.split()]
+
+
+def print_result(lst):
+    return print(*list(map(int, lst)))
+
+
+numbers = take_input()
+numbers_in_reversed = reversed_nums(numbers)
+numbers_ordered = ordered_nums(numbers_in_reversed)
+print_result(numbers_ordered)
+
+
+
+
+
+
+
+# nums = input().split("|")
+# # joined_lists = [pair.strip() for pair in reversed(nums)]
+# joined_lists = [pair for pair in reversed(nums)]
+# flattened_list = [num for sublist in joined_lists for num in sublist.split()]
+# result = list(map(int, flattened_list))
+# print(*result)
