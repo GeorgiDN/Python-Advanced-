b48eee415b1aa4ec41ecf679c7dceececdef7855
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-26 20:54:40 +0200

    Update 4_water_dispenser.py

--- Lists as Stacks and Queues - Lab/4_water_dispenser.py
+++ Lists as Stacks and Queues - Lab/4_water_dispenser.py
@@ -1,41 +1,79 @@
-from _collections import deque
+from collections import deque
 
-def add_names_in_deque():
-    people_data = deque()
-    while True:
-        name = input()
+liters_in_dispenser = int(input())
+people_in_the_queue = deque()
 
-        if name == COMMAND_START:
-            break
-        people_data.append(name)
-
-    return people_data
-
-
-COMMAND_END = 'End'
-COMMAND_START = 'Start'
-COMMAND_REFILL = 'refill '
-water_amount = int(input())
-people_deque = add_names_in_deque()
-
+start = False
 while True:
     command = input()
-
-    if command == COMMAND_END:
-        print(f'{water_amount} liters left')
+    if command == "End":
         break
 
-    elif command.startswith(COMMAND_REFILL):
-        refill_command_data = command.split(' ')
-        refill_water_amount = int(refill_command_data[1])
-        water_amount += refill_water_amount
+    if command == "Start":
+        start = True
+        continue
+
+    if not start:
+        name = command
+        people_in_the_queue.append(name)
 
     else:
-        person = people_deque.popleft()
-        current_litres = int(command)
+        if command.startswith("ref"):
+            data = command.split()
+            liters = int(data[1])
+            liters_in_dispenser += liters
 
-        if current_litres <= water_amount:
-            print(f'{person} got water')
-            water_amount -= current_litres
         else:
-            print(f'{person} must wait')
+            wanted_water = int(command)
+            if wanted_water <= liters_in_dispenser:
+                print(f"{people_in_the_queue.popleft()} got water")
+                liters_in_dispenser -= wanted_water
+            else:
+                print(f"{people_in_the_queue.popleft()} must wait")
+
+print(f"{liters_in_dispenser} liters left")
+
+
+
+
+# from _collections import deque
+
+# def add_names_in_deque():
+#     people_data = deque()
+#     while True:
+#         name = input()
+
+#         if name == COMMAND_START:
+#             break
+#         people_data.append(name)
+
+#     return people_data
+
+
+# COMMAND_END = 'End'
+# COMMAND_START = 'Start'
+# COMMAND_REFILL = 'refill '
+# water_amount = int(input())
+# people_deque = add_names_in_deque()
+
+# while True:
+#     command = input()
+
+#     if command == COMMAND_END:
+#         print(f'{water_amount} liters left')
+#         break
+
+#     elif command.startswith(COMMAND_REFILL):
+#         refill_command_data = command.split(' ')
+#         refill_water_amount = int(refill_command_data[1])
+#         water_amount += refill_water_amount
+
+#     else:
+#         person = people_deque.popleft()
+#         current_litres = int(command)
+
+#         if current_litres <= water_amount:
+#             print(f'{person} got water')
+#             water_amount -= current_litres
+#         else:
+#             print(f'{person} must wait')
