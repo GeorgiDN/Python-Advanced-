ca2cd6eb05033f1bebcebf4ad36ea9ea2fdb920e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-26 20:31:32 +0200

    Update 3_supermarket.py

--- Lists as Stacks and Queues - Lab/3_supermarket.py
+++ Lists as Stacks and Queues - Lab/3_supermarket.py
@@ -1,20 +1,40 @@
-from _collections import deque
+from collections import deque
 
-names_deque = deque()
-COMMAND_END = 'End'
-COMMAND_PAID = 'Paid'
+people_on_the_queue = deque()
 
 while True:
-    command = input()
-
-    if command == COMMAND_END:
-        print(f'{len(names_deque)} people remaining.')
+    name = input()
+    if name == "End":
         break
 
-    elif command == COMMAND_PAID:
-        while names_deque:
-            print(names_deque.popleft())
+    elif name == "Paid":
+        while people_on_the_queue:
+            print(people_on_the_queue.popleft())
 
     else:
-        names_deque.append(command)
+        people_on_the_queue.append(name)
+
+print(f"{len(people_on_the_queue)} people remaining.")
+
+
+
+# from _collections import deque
+
+# names_deque = deque()
+# COMMAND_END = 'End'
+# COMMAND_PAID = 'Paid'
+
+# while True:
+#     command = input()
+
+#     if command == COMMAND_END:
+#         print(f'{len(names_deque)} people remaining.')
+#         break
+
+#     elif command == COMMAND_PAID:
+#         while names_deque:
+#             print(names_deque.popleft())
+
+#     else:
+#         names_deque.append(command)
       
