0292746906a4124bbfad8b78db17a4bc07c3c1be
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-28 21:51:53 +0200

    Update 8_cross_roads.py

--- Lists as Stacks and Queues - Exercise/8_cross_roads.py
+++ Lists as Stacks and Queues - Exercise/8_cross_roads.py
@@ -1,37 +1,82 @@
 from collections import deque
 
-green_light = int(input())
+green_light_time = int(input())
 free_window = int(input())
-time_to_enter = green_light
-passed_time = 0
+cars_on_crossroad = deque()
 
-cars = deque()
 passed_cars = 0
-crash = False
+crash_happened = False
 
 while True:
-    command = input()
-    if command == "END":
-        print("Everyone is safe.")
-        print(f"{passed_cars} total cars passed the crossroads.")
+    info = input()
+    if info == "END" or crash_happened:
         break
-    elif command == "green":
-        while cars:
-            if time_to_enter <= 0:
-                break
-            current_car = cars.popleft()
-            car_size = len(current_car)
-            available_time = time_to_enter + free_window
-            if car_size <= available_time:
+
+    if info == "green":
+        current_green_time = green_light_time
+        current_free_window = free_window
+
+        while cars_on_crossroad and not crash_happened and current_green_time > 0:
+            current_car = cars_on_crossroad.popleft()
+            for char in current_car:
+                if current_green_time > 0:
+                    current_green_time -= 1
+                else:
+                    if current_free_window > 0:
+                        current_free_window -= 1
+                    else:
+                        crash_happened = True
+                        print("A crash happened!")
+                        print(f"{current_car} was hit at {char}.")
+                        break
+            if not crash_happened:
                 passed_cars += 1
-                time_to_enter -= len(current_car)
-            else:
-                crash = True
-                print("A crash happened!")
-                print(f"{current_car} was hit at {current_car[available_time]}.")
-                break
-        time_to_enter = green_light
     else:
-        cars.append(command)
-    if crash:
-        break
+        car = info
+        cars_on_crossroad.append(car)
+
+if not crash_happened:
+    print("Everyone is safe.")
+    print(f"{passed_cars} total cars passed the crossroads.")
+
+
+
+
+# from collections import deque
+
+# green_light = int(input())
+# free_window = int(input())
+# time_to_enter = green_light
+# passed_time = 0
+
+# cars = deque()
+# passed_cars = 0
+# crash = False
+
+# while True:
+#     command = input()
+#     if command == "END":
+#         print("Everyone is safe.")
+#         print(f"{passed_cars} total cars passed the crossroads.")
+#         break
+#     elif command == "green":
+#         while cars:
+#             if time_to_enter <= 0:
+#                 break
+#             current_car = cars.popleft()
+#             car_size = len(current_car)
+#             available_time = time_to_enter + free_window
+#             if car_size <= available_time:
+#                 passed_cars += 1
+#                 time_to_enter -= len(current_car)
+#             else:
+#                 crash = True
+#                 print("A crash happened!")
+#                 print(f"{current_car} was hit at {current_car[available_time]}.")
+#                 break
+#         time_to_enter = green_light
+#     else:
+#         cars.append(command)
+#     if crash:
+#         break
+        
