09ff9db3a1f102be4c44feaecb97f712fec3b619
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-28 22:39:54 +0200

    Update 10_cups_and_bottles.py

--- Lists as Stacks and Queues - Exercise/10_cups_and_bottles.py
+++ Lists as Stacks and Queues - Exercise/10_cups_and_bottles.py
@@ -1,24 +1,49 @@
 from collections import deque
 
-cups = deque(list(map(int, input().split())))
-bottles = list(map(int, input().split()))
-wasted_water = 0
+cups = deque(map(int, input().split(" ")))
+bottles = list(map(int, input().split(" ")))
+wasted_liters_of_water = 0
 
-while True:
-    if not cups or not bottles:
-        break
-    cup = cups[0]
+while cups and bottles:
+    cup = cups.popleft()
     bottle = bottles.pop()
-    if cup - bottle <= 0:
-        cups.popleft()
-        wasted_water += abs(cup - bottle)
+    if bottle >= cup:
+        bottle -= cup
+        wasted_liters_of_water += bottle
     else:
-        cups[0] -= bottle
+        cup -= bottle
+        cups.insert(0, cup)
 
-if cups:
-    cups_list = [str(i) for i in cups]
-    print(f"Cups: {' '.join(cups_list)}")
+if bottles:
+    print(f"Bottles: {' '.join(map(str, bottles))}")
 else:
-    bottles_list = [str(i) for i in bottles]
-    print(f"Bottles: {' '.join(bottles_list)}")
-print(f"Wasted litters of water: {wasted_water}")
+    print(f"Cups: {' '.join(map(str, cups))}")
+print(f"Wasted litters of water: {wasted_liters_of_water}")
+
+
+
+
+# from collections import deque
+
+# cups = deque(list(map(int, input().split())))
+# bottles = list(map(int, input().split()))
+# wasted_water = 0
+
+# while True:
+#     if not cups or not bottles:
+#         break
+#     cup = cups[0]
+#     bottle = bottles.pop()
+#     if cup - bottle <= 0:
+#         cups.popleft()
+#         wasted_water += abs(cup - bottle)
+#     else:
+#         cups[0] -= bottle
+
+# if cups:
+#     cups_list = [str(i) for i in cups]
+#     print(f"Cups: {' '.join(cups_list)}")
+# else:
+#     bottles_list = [str(i) for i in bottles]
+#     print(f"Bottles: {' '.join(bottles_list)}")
+# print(f"Wasted litters of water: {wasted_water}")
