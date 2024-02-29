2624e3baea478c166617cb0950fcdd6ff6c28851
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 21:43:48 +0200

    Update 1_count_same_values.py

--- Tuples and Sets - Lab/1_count_same_values.py
+++ Tuples and Sets - Lab/1_count_same_values.py
@@ -1,3 +1,5 @@
+# https://judge.softuni.org/Contests/Practice/Index/1832#0
+
 from collections import Counter
 
 list_numbers = [float(x) for x in input().split()]
@@ -5,31 +7,41 @@ occurrences = Counter(list_numbers)
 print("\n".join([f"{x} - {y} times" for x, y in occurrences.items()]))
 
 
+#############################################################################
+# from collections import Counter
+# print("\n".join([f"{x} - {y} times" for x, y in Counter([float(n) for n in input().split()]).items()]))
 
 
+
+#############################################################################
+# from collections import Counter
+# list_numbers = [float(x) for x in input().split()]
+# print("\n".join([f"{x} - {y} times" for x, y in Counter(list_numbers).items()]))
+
+
+
+#############################################################################
 # list_numbers = [float(x) for x in input().split()]
 # occurrences = {}
-
+#
 # for num in list_numbers:
 #     if num not in occurrences:
 #         occurrences[num] = 0
 #     occurrences[num] += 1
-
+#
 # print("\n".join([f"{x} - {y} times" for x, y in occurrences.items()]))
 
 
 
-
-
+#############################################################################
 # numbers = list(map(float, input().split(" ")))
 # numbers_counter = {}
-
+#
 # for num in numbers:
 #     if num not in numbers_counter:
 #         numbers_counter[num] = 1
 #     else:
 #         numbers_counter[num] += 1
-
+#
 # for key, value in numbers_counter.items():
 #     print(f"{key} - {value} times")
-  
