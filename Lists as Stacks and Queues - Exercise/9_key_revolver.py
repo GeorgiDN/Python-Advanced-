6fbe17f574f56328cc4132c0ed987e65cac918ab
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 20:42:59 +0200

    Update 9_key_revolver.py

--- Lists as Stacks and Queues - Exercise/9_key_revolver.py
+++ Lists as Stacks and Queues - Exercise/9_key_revolver.py
@@ -2,28 +2,27 @@ from collections import deque
 
 bullet_price = int(input())
 gun_barrel_size = int(input())
-bullets = list(map(int, input().split()))       # bullets are a stack
-locks = deque(list(map(int, input().split())))  # locks are a queue
-prize_value = int(input())
-shots = 0
+bullets = list(map(int, input().split()))       
+locks = deque(list(map(int, input().split()))) 
+prize_of_the_value = int(input())
+number_of_shots = 0
 
 while bullets:
-    if len(locks) == 0:
+    if not locks:
         break
-    shots += 1
-    current_bullet = bullets.pop()
-    prize_value -= bullet_price
-    if current_bullet <= locks[0]:
+    number_of_shots += 1
+    bullet = bullets.pop()
+    prize_of_the_value -= bullet_price
+    if bullet <= locks[0]:
         print("Bang!")
         locks.popleft()
     else:
         print("Ping!")
-    if shots == gun_barrel_size and bullets:
+    if number_of_shots == gun_barrel_size and bullets:
         print("Reloading!")
-        shots = 0
+        number_of_shots = 0
 
 if locks:
     print(f"Couldn't get through. Locks left: {len(locks)}")
 else:
-    print(f"{len(bullets)} bullets left. Earned ${prize_value}")
-    
+    print(f"{len(bullets)} bullets left. Earned ${prize_of_the_value}")
