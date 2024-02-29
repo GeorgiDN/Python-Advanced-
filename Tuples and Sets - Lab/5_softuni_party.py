f0446f80e9dabe66365612a2abbbe0bbb624246a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 22:50:02 +0200

    Update 5_softuni_party.py

--- Tuples and Sets - Lab/5_softuni_party.py
+++ Tuples and Sets - Lab/5_softuni_party.py
@@ -1,40 +1,77 @@
-number = int(input())
+number_of_guests = int(input())
+vip_guests = []
+regular_guests = []
 
-vip = []
-regular = []
-
-for i in range(number):
-    reservation = input()
-
-    if reservation[0].isdigit():
-        if reservation not in vip:
-            vip.append(reservation)
+for _ in range(number_of_guests):
+    reservation_code = input()
+    if reservation_code[0].isdigit():
+        if reservation_code not in vip_guests:
+            vip_guests.append(reservation_code)
     else:
-        if reservation not in regular:
-            regular.append(reservation)
+        if reservation_code not in regular_guests:
+            regular_guests.append(reservation_code)
 
 while True:
-    command = input()
-    if command == "END":
+    reservation_code = input()
+    if reservation_code == "END":
         break
 
-    guest = command
+    if reservation_code in vip_guests:
+        vip_guests.remove(reservation_code)
 
-    if guest in vip:
-        vip.remove(guest)
-    elif guest in regular:
-        regular.remove(guest)
+    elif reservation_code in regular_guests:
+        regular_guests.remove(reservation_code)
 
-total = len(vip) + len(regular)
-vip = sorted(vip)
-regular = sorted(regular)
+number_guest_not_coming = len(vip_guests) + len(regular_guests)
+print(number_guest_not_coming)
 
-print(total)
+if vip_guests:
+    vip_guests = sorted(vip_guests)
+    print('\n'.join([guest for guest in vip_guests]))
+if regular_guests:
+    regular_guests = sorted(regular_guests)
+    print('\n'.join([guest for guest in regular_guests]))
 
-if vip:
-    for v in vip:
-        print(''.join(v))
-if regular:
-    for r in regular:
-        print(''.join(r))
+
+
+
+# number = int(input())
+
+# vip = []
+# regular = []
+
+# for i in range(number):
+#     reservation = input()
+
+#     if reservation[0].isdigit():
+#         if reservation not in vip:
+#             vip.append(reservation)
+#     else:
+#         if reservation not in regular:
+#             regular.append(reservation)
+
+# while True:
+#     command = input()
+#     if command == "END":
+#         break
+
+#     guest = command
+
+#     if guest in vip:
+#         vip.remove(guest)
+#     elif guest in regular:
+#         regular.remove(guest)
+
+# total = len(vip) + len(regular)
+# vip = sorted(vip)
+# regular = sorted(regular)
+
+# print(total)
+
+# if vip:
+#     for v in vip:
+#         print(''.join(v))
+# if regular:
+#     for r in regular:
+#         print(''.join(r))
       
