2ad8833d5a393c9690160c1fd70e34dce476c985
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 22:13:59 +0200

    Update 3_record_unique_names.py

--- Tuples and Sets - Lab/3_record_unique_names.py
+++ Tuples and Sets - Lab/3_record_unique_names.py
@@ -4,3 +4,7 @@ names_data = {input() for _ in range(n)}
 for name in names_data:
     print(name)
   
+
+
+# names_number = int(input())
+# print('\n'.join([name for name in {input() for _ in range(names_number)}]))
