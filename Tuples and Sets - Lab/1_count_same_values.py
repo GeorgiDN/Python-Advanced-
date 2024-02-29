fdec086f8a309a5fed293003b67b02d87aed518d
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 21:28:22 +0200

    Update 1_count_same_values.py

--- Tuples and Sets - Lab/1_count_same_values.py
+++ Tuples and Sets - Lab/1_count_same_values.py
@@ -1,12 +1,24 @@
-numbers = list(map(float, input().split(" ")))
-numbers_counter = {}
+list_numbers = [float(x) for x in input().split()]
+occurrences = {}
 
-for num in numbers:
-    if num not in numbers_counter:
-        numbers_counter[num] = 1
-    else:
-        numbers_counter[num] += 1
+for num in list_numbers:
+    if num not in occurrences:
+        occurrences[num] = 0
+    occurrences[num] += 1
 
-for key, value in numbers_counter.items():
-    print(f"{key} - {value} times")
+print("\n".join([f"{x} - {y} times" for x, y in occurrences.items()]))
+
+
+
+# numbers = list(map(float, input().split(" ")))
+# numbers_counter = {}
+
+# for num in numbers:
+#     if num not in numbers_counter:
+#         numbers_counter[num] = 1
+#     else:
+#         numbers_counter[num] += 1
+
+# for key, value in numbers_counter.items():
+#     print(f"{key} - {value} times")
   
