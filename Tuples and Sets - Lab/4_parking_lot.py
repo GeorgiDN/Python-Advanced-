9d057a04cc6073e3001169f725b5f738b4e27844
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 22:28:05 +0200

    Update 4_parking_lot.py

--- Tuples and Sets - Lab/4_parking_lot.py
+++ Tuples and Sets - Lab/4_parking_lot.py
@@ -1,22 +1,65 @@
-commands_number = int(input())
+number_of_commands = int(input())
+parking_lot = []
 
-parking = []
-for i in range(commands_number):
-    info = input().split(", ")
-    command = info[0]
-    number = info[1]
+for _ in range(number_of_commands):
+    command, current_number = input().split(", ")
+    if command == "IN":
+        parking_lot.append(current_number)
+    elif command == "OUT":
+        if parking_lot:
+            parking_lot.remove(current_number)
 
-    if command == 'IN':
-        if number not in parking:
-            parking.append(number)
-    elif command == 'OUT':
-        if number in parking:
-            parking.remove(number)
+if parking_lot:
+    print('\n'.join([num for num in set(parking_lot)]))
 
-if parking:
-    for curr_num in parking:
-        print(''.join(curr_num))
+else:
+    print("Parking Lot is Empty")
 
-elif not parking:
-    print('Parking Lot is Empty')
+
+
+
+# number_of_commands = int(input())
+# parking_lot = []
+#
+# for _ in range(number_of_commands):
+#     command, current_number = input().split(", ")
+#     if command == "IN":
+#         if current_number not in parking_lot:
+#             parking_lot.append(current_number)
+#     elif command == "OUT":
+#         if parking_lot and current_number in parking_lot:
+#             parking_lot.remove(current_number)
+#
+# if parking_lot:
+#     print('\n'.join([num for num in parking_lot]))
+#
+# else:
+#     print("Parking Lot is Empty")
+
+
+
+
+
+
+# commands_number = int(input())
+
+# parking = []
+# for i in range(commands_number):
+#     info = input().split(", ")
+#     command = info[0]
+#     number = info[1]
+
+#     if command == 'IN':
+#         if number not in parking:
+#             parking.append(number)
+#     elif command == 'OUT':
+#         if number in parking:
+#             parking.remove(number)
+
+# if parking:
+#     for curr_num in parking:
+#         print(''.join(curr_num))
+
+# elif not parking:
+#     print('Parking Lot is Empty')
   
