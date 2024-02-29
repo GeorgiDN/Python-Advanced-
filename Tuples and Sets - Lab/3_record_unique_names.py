db2d1090498a3835cf41953769f8921f9586b121
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 22:15:20 +0200

    Update 3_record_unique_names.py

--- Tuples and Sets - Lab/3_record_unique_names.py
+++ Tuples and Sets - Lab/3_record_unique_names.py
@@ -1,10 +1,25 @@
-n = int(input())
-names_data = {input() for _ in range(n)}
+names_number = int(input())
+names_data = {input() for _ in range(names_number)}
 
 for name in names_data:
     print(name)
-  
+
 
 
 # names_number = int(input())
 # print('\n'.join([name for name in {input() for _ in range(names_number)}]))
+
+
+
+
+# number_names = int(input())
+# number_list = []
+#
+# for name in range(number_names):
+#     curr_name = input()
+#     number_list.append(curr_name)
+#
+# not_repeat_names = set(number_list)
+# for n in not_repeat_names:
+#     print(n)
+#
