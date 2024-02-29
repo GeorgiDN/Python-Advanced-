abee17c155df27543e89b7031a090b2185e171d3
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-02-29 22:03:24 +0200

    Update 2_students'_grade.py

--- Tuples and Sets - Lab/2_students'_grade.py
+++ Tuples and Sets - Lab/2_students'_grade.py
@@ -2,19 +2,38 @@ number_of_students = int(input())
 students_records = {}
 
 for _ in range(number_of_students):
-    name, assessment = input().split()
-    assessment = float(assessment)
+    name, grade = input().split()
+    grade = float(grade)
 
     if name not in students_records:
         students_records[name] = []
-    students_records[name].append(assessment)
+    students_records[name].append(grade)
 
 for curr_name, marks in students_records.items():
     average_grade = sum(marks) / len(marks)
-    formatted_assessments = []
-    for mark in marks:
-        formatted_assessments.append(f"{mark:.2f}")
-    print(f"{curr_name} -> {' '.join(map(str, formatted_assessments))} (avg: {average_grade:.2f})")
+    formatted_grades = [f"{mark:.2f}" for mark in marks]
+    print(f"{curr_name} -> {' '.join(map(str, formatted_grades))} (avg: {average_grade:.2f})")
+
+
+
+# number_of_students = int(input())
+# students_records = {}
+
+# for _ in range(number_of_students):
+#     name, assessment = input().split()
+#     assessment = float(assessment)
+
+#     if name not in students_records:
+#         students_records[name] = []
+#     students_records[name].append(assessment)
+
+# for curr_name, marks in students_records.items():
+#     average_grade = sum(marks) / len(marks)
+#     formatted_assessments = []
+#     for mark in marks:
+#         formatted_assessments.append(f"{mark:.2f}")
+#     print(f"{curr_name} -> {' '.join(map(str, formatted_assessments))} (avg: {average_grade:.2f})")
+
 
 
 
