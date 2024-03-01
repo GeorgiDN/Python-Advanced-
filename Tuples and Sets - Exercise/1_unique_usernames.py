6a79f6eb8b4a3175d587476b5bb588bb36f17b62
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-01 18:58:51 +0200

    Update 1_unique_usernames.py

--- Tuples and Sets - Exercise/1_unique_usernames.py
+++ Tuples and Sets - Exercise/1_unique_usernames.py
@@ -1,9 +1,14 @@
-number = int(input())
-unique_names = [input() for _ in range(number)]
-set_unique_names = set(unique_names)
+number_names = int(input())
+print('\n'.join(set([name for _ in range(number_names) for name in input().split()])))
 
-for name in set_unique_names:
-    print(name)
+
+# number = int(input())
+# unique_names = [input() for _ in range(number)]
+# set_unique_names = set(unique_names)
+
+# for name in set_unique_names:
+#     print(name)
+
 
 
 # def input_lines(num):
