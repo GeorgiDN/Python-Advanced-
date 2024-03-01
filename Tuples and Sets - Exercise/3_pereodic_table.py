ffd68ea27f6ae983eff6b94d615c2bbcf3f7bb54
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-01 19:57:03 +0200

    Update 3_pereodic_table.py

--- Tuples and Sets - Exercise/3_pereodic_table.py
+++ Tuples and Sets - Exercise/3_pereodic_table.py
@@ -1,11 +1,42 @@
-n = int(input())
+rows = int(input())
 
-unique = set()
-for i in range(n):
-    chemical_compounds = input().split(' ')
-    for chem in chemical_compounds:
-        unique.add(chem)
+unique_chemical_elements = set()
+for _ in range(rows):
+    elements = input().split()
+    for element in elements:
+        unique_chemical_elements.add(element)
 
-for char in unique:
-    print(char)
-  
+[print(el) for el in unique_chemical_elements]
+
+
+
+# rows = int(input())
+#
+# all_elements = []
+#
+# for _ in range(rows):
+#     elements = input().split()
+#     for element in elements:
+#         if element not in all_elements:
+#             all_elements.append(element)
+#
+# [print(el) for el in all_elements]
+
+
+# rows = int(input())
+# print('\n'.join(map(str, {element for _ in range(rows) for element in input().split()})))
+
+
+
+
+
+# n = int(input())
+# 
+# unique = set()
+# for i in range(n):
+#     chemical_compounds = input().split(' ')
+#     for chem in chemical_compounds:
+#         unique.add(chem)
+# 
+# for char in unique:
+#     print(char)
