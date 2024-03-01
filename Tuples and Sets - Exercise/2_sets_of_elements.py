7b91237d53361d2e9545f1a8c64ac7df7f88a280
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-01 19:33:56 +0200

    Update 2_sets_of_elements.py

--- Tuples and Sets - Exercise/2_sets_of_elements.py
+++ Tuples and Sets - Exercise/2_sets_of_elements.py
@@ -1,17 +1,17 @@
 length_of_two = input().split(' ')
-number_1 = int(length_of_two[0])
-number_2 = int(length_of_two[1])
+first_set_length = int(length_of_two[0])
+second_set_length = int(length_of_two[1])
 
 list_1 = []
 list_2 = []
 repeat = []
 
-for i in range(number_1):
+for num1 in range(first_set_length):
     number = int(input())
     if number not in list_1:
         list_1.append(number)
 
-for j in range(number_2):
+for num2 in range(second_set_length):
     number2 = int(input())
     if number2 not in list_2:
         list_2.append(number2)
@@ -20,8 +20,8 @@ for num in list_1:
     if num in list_2:
         repeat.append(num)
 
-for curr_num in repeat:
-    print(''.join(str(curr_num)))
+
+print('\n'.join(map(str, [curr_num for curr_num in repeat])))
   
 
 
