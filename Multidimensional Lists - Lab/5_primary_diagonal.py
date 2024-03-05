ab236d4be4477564d86d39f3d0224e11e0e24c6b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-05 21:23:30 +0200

    Update 5_primary_diagonal.py

--- Multidimensional Lists - Lab/5_primary_diagonal.py
+++ Multidimensional Lists - Lab/5_primary_diagonal.py
@@ -1,8 +1,45 @@
-n = int(input())
-matrix = []
-
-for _ in range(n):
-    matrix.append([int(x) for x in input().split()])
-
-primary_diagonal_sum = sum([matrix[i][i] for i in range(n)])
+rows = int(input())
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
+primary_diagonal_sum = sum([sum(matrix[i][i] for i in range(rows))])
 print(primary_diagonal_sum)
+
+
+
+# IT DOES NOT WORK IN JUDGE SYSTEM THIS SYNTAX -> :=
+# rows = int(input())
+# matrix_and_diagonal_sum = (
+#     (matrix := [[int(x) for x in input().split()] for _ in range(rows)]),
+#     (diagonal_sum := sum(matrix[i][i] for i in range(rows))),
+# )[1]
+#
+# print(matrix_and_diagonal_sum)
+
+
+
+
+# n = int(input())
+# matrix = []
+#
+# for _ in range(n):
+#     matrix.append([int(x) for x in input().split()])
+#
+# primary_diagonal_sum = sum([matrix[i][i] for i in range(n)])
+# print(primary_diagonal_sum)
+
+
+
+
+# n = int(input())
+# matrix = []
+# primary_diagonal_sum = []
+# idx = 0
+# for _ in range(n):
+#     matrix.append([int(x) for x in input().split()])
+#
+# for col in range(1):
+#     for row in matrix:
+#         column = row[col + idx]
+#         primary_diagonal_sum.append(column)
+#         idx += 1
+#     print(sum(primary_diagonal_sum))
+#     break
