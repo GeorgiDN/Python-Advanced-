3f1cafd297764c9350e6bc3f21bde7db36e12ea2
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-05 20:11:14 +0200

    Update 3_flattening_matrix.py

--- Multidimensional Lists - Lab/3_flattening_matrix.py
+++ Multidimensional Lists - Lab/3_flattening_matrix.py
@@ -1,8 +1,23 @@
 rows = int(input())
-matrix = []
-
-for _ in range(rows):
-    numbers = [int(x) for x in input().split(", ")]
-    matrix.extend(numbers)
-
+matrix = [int(x) for _ in range(rows) for x in input().split(", ")]
 print(matrix)
+
+
+
+# rows = int(input())
+# print([int(x) for _ in range(rows) for x in input().split(", ")])
+
+
+
+# print([int(x) for _ in range(int(input())) for x in input().split(", ")])
+
+
+
+# rows = int(input())
+# matrix = []
+# 
+# for _ in range(rows):
+#     numbers = [int(x) for x in input().split(", ")]
+#     matrix.extend(numbers)
+# 
+# print(matrix)
