4fb5b97a5110ca25bc4afd93179d32e241455fed
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-05 20:46:31 +0200

    Update 4_sum_matrix_columns.py

--- Multidimensional Lists - Lab/4_sum_matrix_columns.py
+++ Multidimensional Lists - Lab/4_sum_matrix_columns.py
@@ -1,9 +1,44 @@
-rows, columns = [int(x) for x in input().split(", ")]
-matrix = []
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split()])
+rows, cols = [int(x) for x in input().split(", ")]
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
-for col in range(columns):
-    column = [row[col] for row in matrix]
-    print(sum(column))
-  
+print('\n'.join(map(str, [sum(row[col] for row in matrix) for col in range(cols)])))
+
+
+
+# rows, cols = [int(x) for x in input().split(", ")]
+# matrix = [[int(x) for x in input().split()] for _ in range(rows)]
+#
+# for col in range(cols):
+#     sum_cols = 0
+#     for row in matrix:
+#         sum_cols += row[col]
+#     print(sum_cols)
+
+
+
+
+# rows, columns = [int(x) for x in input().split(", ")]
+# matrix = []
+# for _ in range(rows):
+#     matrix.append([int(x) for x in input().split()])
+#
+# for col in range(columns):
+#     column = [row[col] for row in matrix]
+#     print(sum(column))
+
+
+
+
+# rows, columns = [int(x) for x in input().split(", ")]
+# matrix = []
+# result = []
+# for _ in range(rows):
+#     matrix.append([int(x) for x in input().split()])
+#
+# for col in range(columns):
+#     for row in matrix:
+#         column = row[col]
+#         result.append(column)
+#
+#     print(sum(result))
+#     result = []
