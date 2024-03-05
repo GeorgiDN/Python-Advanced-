86f3f23f7d2636b2687d785624ada322a2b16e0b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-05 20:04:25 +0200

    Update 1_sum_matrix_elements.py

--- Multidimensional Lists - Lab/1_sum_matrix_elements.py
+++ Multidimensional Lists - Lab/1_sum_matrix_elements.py
@@ -1,14 +1,23 @@
-rows, columns = [int(x) for x in input().split(", ")]
-matrix = []
-total_sum = 0
-
-for _ in range(rows):
-    numbers = [int(x) for x in input().split(", ")]
-    matrix.append(numbers)
-
-for row in range(rows):
-    for column in range(columns):
-        total_sum += matrix[row][column]
+rows, cols = [int(x) for x in input().split(", ")]
+matrix = [[int(x) for x in input().split(", ")] for _ in range(rows)]
+total_sum = sum([num for row in matrix for num in row])
 
 print(total_sum)
 print(matrix)
+
+
+
+# rows, columns = [int(x) for x in input().split(", ")]
+# matrix = []
+# total_sum = 0
+
+# for _ in range(rows):
+#     numbers = [int(x) for x in input().split(", ")]
+#     matrix.append(numbers)
+
+# for row in range(rows):
+#     for column in range(columns):
+#         total_sum += matrix[row][column]
+
+# print(total_sum)
+# print(matrix)
