17ce76a7f9e30311e9de8de2a438578699173a0f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-07 21:37:33 +0200

    Update 1_diagonals.py

--- Multidimensional Lists - Exercise 1/1_diagonals.py
+++ Multidimensional Lists - Exercise 1/1_diagonals.py
@@ -1,16 +1,31 @@
 rows = int(input())
-matrix = []
+matrix = [[int(x) for x in input().split(", ")] for _ in range(rows)]
 
-primary_diagonal_numbers = []
-secondary_diagonal_numbers = []
+primary_diagonal_numbers = ([matrix[i][i] for i in range(rows)])
+primary_diagonal_sum = sum(primary_diagonal_numbers)
 
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split(", ")])
+secondary_diagonal_numbers = ([matrix[i][rows - 1 - i] for i in range(rows)])
+secondary_diagonal_sum = sum(secondary_diagonal_numbers)
 
-for i in range(rows):
-    primary_diagonal_numbers.append(matrix[i][i])
-    secondary_diagonal_numbers.append(matrix[i][(rows - 1) - i])
+print(f"Primary diagonal: {', '.join(map(str, primary_diagonal_numbers))}. Sum: {primary_diagonal_sum}")
+print(f"Secondary diagonal: {', '.join(map(str, secondary_diagonal_numbers))}. Sum: {secondary_diagonal_sum}")
 
 
-print(f"Primary diagonal: {', '.join(map(str, primary_diagonal_numbers))}. Sum: {sum(primary_diagonal_numbers)}")
-print(f"Secondary diagonal: {', '.join(map(str, secondary_diagonal_numbers))}. Sum: {sum(secondary_diagonal_numbers)}")
+
+
+# rows = int(input())
+# matrix = []
+
+# primary_diagonal_numbers = []
+# secondary_diagonal_numbers = []
+
+# for _ in range(rows):
+#     matrix.append([int(x) for x in input().split(", ")])
+
+# for i in range(rows):
+#     primary_diagonal_numbers.append(matrix[i][i])
+#     secondary_diagonal_numbers.append(matrix[i][(rows - 1) - i])
+
+
+# print(f"Primary diagonal: {', '.join(map(str, primary_diagonal_numbers))}. Sum: {sum(primary_diagonal_numbers)}")
+# print(f"Secondary diagonal: {', '.join(map(str, secondary_diagonal_numbers))}. Sum: {sum(secondary_diagonal_numbers)}")
