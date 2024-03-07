81e662859b397e99be539a43d1a81a232cad0e66
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-07 21:44:53 +0200

    Update 2_diagonal_difference.py

--- Multidimensional Lists - Exercise 1/2_diagonal_difference.py
+++ Multidimensional Lists - Exercise 1/2_diagonal_difference.py
@@ -1,15 +1,31 @@
 rows = int(input())
-matrix = []
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
-primary_diagonal_numbers = []
-secondary_diagonal_numbers = []
+primary_diagonal_numbers = ([matrix[i][i] for i in range(rows)])
+primary_diagonal_sum = sum(primary_diagonal_numbers)
 
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split(" ")])
+secondary_diagonal_numbers = ([matrix[i][rows - 1 - i] for i in range(rows)])
+secondary_diagonal_sum = sum(secondary_diagonal_numbers)
 
-for i in range(rows):
-    primary_diagonal_numbers.append(matrix[i][i])
-    secondary_diagonal_numbers.append(matrix[i][(rows - 1) - i])
+difference = abs(primary_diagonal_sum - secondary_diagonal_sum)
+print(difference)
 
-diff = abs(sum(primary_diagonal_numbers) - sum(secondary_diagonal_numbers))
-print(diff)
+
+
+
+
+# rows = int(input())
+# matrix = []
+
+# primary_diagonal_numbers = []
+# secondary_diagonal_numbers = []
+
+# for _ in range(rows):
+#     matrix.append([int(x) for x in input().split(" ")])
+
+# for i in range(rows):
+#     primary_diagonal_numbers.append(matrix[i][i])
+#     secondary_diagonal_numbers.append(matrix[i][(rows - 1) - i])
+
+# diff = abs(sum(primary_diagonal_numbers) - sum(secondary_diagonal_numbers))
+# print(diff)
