5f4362e645e35e6a7467dd7da2b61bba5005ab30
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-07 22:38:12 +0200

    Update 5_matrix_of_palindromes.py

--- Multidimensional Lists - Exercise 1/5_matrix_of_palindromes.py
+++ Multidimensional Lists - Exercise 1/5_matrix_of_palindromes.py
@@ -1,15 +1,30 @@
-
-rows, columns = [int(x) for x in input().split()]
-matrix = []
+rows, cols = [int(x) for x in input().split()]
+matrix_of_palindromes = []
 
 for row in range(rows):
-    current_row = []
-    for col in range(columns):
-        first_letter = chr(97 + row)
-        second_letter = chr(97 + row + col)
-        third_letter = first_letter
-        palindrome = first_letter + second_letter + third_letter
-        current_row.append(palindrome)
-    matrix.append(current_row)
+    curr_row = []
+    for col in range(cols):
+        curr_string = chr(97 + row) + chr(97 + row + col) + chr(97 + row)
+        if curr_string == curr_string[::-1]:
+            curr_row.append(curr_string)
+    matrix_of_palindromes.append(curr_row)
 
-[print(" ".join(el)) for el in matrix]
+for palindrome in matrix_of_palindromes:
+    print(' '.join(palindrome))
+
+
+
+# rows, columns = [int(x) for x in input().split()]
+# matrix = []
+
+# for row in range(rows):
+#     current_row = []
+#     for col in range(columns):
+#         first_letter = chr(97 + row)
+#         second_letter = chr(97 + row + col)
+#         third_letter = first_letter
+#         palindrome = first_letter + second_letter + third_letter
+#         current_row.append(palindrome)
+#     matrix.append(current_row)
+
+# [print(" ".join(el)) for el in matrix]
