bb6c05d9ea76d02fbd2e619bffb5b802717d8064
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-08 20:56:45 +0200

    Update 7_snake_moves.py

--- Multidimensional Lists - Exercise 1/7_snake_moves.py
+++ Multidimensional Lists - Exercise 1/7_snake_moves.py
@@ -1,17 +1,36 @@
-from collections import deque
+rows, cols = [int(x) for x in input().split()]
+text = input()
 
-rows, columns = [int(x) for x in input().split()]
-snake = deque(input())
-matrix = []
+result = []
+index = 0
 
 for row in range(rows):
-    current_row = ""
-    for col in range(columns):
-        current_symbol = snake.popleft()
-        current_row += current_symbol
-        snake.append(current_symbol)
-    if row % 2 != 0:
-        current_row = current_row[::-1]
-    matrix.append(current_row)
+    curr_string = ''
+    for col in range(cols):
+        curr_string += text[index % len(text)]
+        index += 1
+    if row > 0 and row % 2 != 0:
+        curr_string = curr_string[::-1]
+    result.append(curr_string)
 
-[print(element) for element in matrix]
+print('\n'.join(result))
+
+
+
+# from collections import deque
+
+# rows, columns = [int(x) for x in input().split()]
+# snake = deque(input())
+# matrix = []
+
+# for row in range(rows):
+#     current_row = ""
+#     for col in range(columns):
+#         current_symbol = snake.popleft()
+#         current_row += current_symbol
+#         snake.append(current_symbol)
+#     if row % 2 != 0:
+#         current_row = current_row[::-1]
+#     matrix.append(current_row)
+
+# [print(element) for element in matrix]
