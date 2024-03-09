7c033f7e67c056f15fb41291d415c9c7ce0e0967
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-09 21:53:53 +0200

    Update 2_matrix_modification.py

--- Multidimensional Lists - Exercise 2/2_matrix_modification.py
+++ Multidimensional Lists - Exercise 2/2_matrix_modification.py
@@ -1,55 +1,40 @@
-rows = int(input())
-matrix = []
+def is_valid_index(idx, number):
+    return 0 <= idx < number
 
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split()])
+
+def add(mtrx, curr_row, col, val, size):
+    if is_valid_index(curr_row, size) and is_valid_index(col, size):
+        mtrx[curr_row][col] += val
+    else:
+        print(f"Invalid coordinates")
+    return mtrx
+
+
+def subtract(mtrx, curr_row, col, val, size):
+    if is_valid_index(curr_row, size) and is_valid_index(col, size):
+        mtrx[curr_row][col] -= val
+    else:
+        print(f"Invalid coordinates")
+    return mtrx
+
+
+rows = int(input())
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
 while True:
     command = input()
     if command == "END":
         break
 
-    command = command.split()
-    operator = command[0]
-    row = int(command[1])
-    col = int(command[2])
-    val = int(command[3])
-    if row in range(rows) and col in range(rows):
-        if operator == 'Add':
-            matrix[row][col] += val
-        elif operator == "Subtract":
-            matrix[row][col] -= val
-    else:
-        print('Invalid coordinates')
+    data = command.split()
+    current_command, row, column, value = data[0], int(data[1]), int(data[2]), int(data[3])
 
-for el in matrix:
-    print(*el)
+    if current_command == "Add":
+        matrix = add(matrix, row, column, value, rows)
+    elif current_command == "Subtract":
+        matrix = subtract(matrix, row, column, value, rows)
 
+print('\n'.join(' '.join(map(str, r)) for r in matrix))
 
-
-
-# def matrix_modification(modification, r, c, val):
-#     if r in range(size) and c in range(size):
-#         if modification == "Add":
-#             matrix[r][c] += val
-#         elif modification == "Subtract":
-#             matrix[r][c] -= val
-#     else:
-#         print("Invalid coordinates")
-
-
-# size = int(input())
-# matrix = []
-
-# for _ in range(size):
-#     matrix.append([int(x) for x in input().split()])
-
-# while True:
-#     command = input()
-#     if command.startswith("END"):
-#         break
-#     action = command.split()[0]
-#     row, col, value = [int(x) for x in command.split()[1:]]
-#     matrix_modification(action, row, col, value)
-
-# [print(" ".join([str(x) for x in element])) for element in matrix]
+# for r in matrix:
+#     print(" ".join(map(str, r)))
