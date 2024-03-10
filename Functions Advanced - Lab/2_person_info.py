2c09771d696746c7e6b898287b767a5dd230763a
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-10 20:48:18 +0200

    Update 2_person_info.py

--- Functions Advanced - Lab/2_person_info.py
+++ Functions Advanced - Lab/2_person_info.py
@@ -3,3 +3,23 @@ def get_info(**kwargs):
 
 
 print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+
+
+
+# def get_info(**kwargs):
+#     person_info = {}
+#     for key, value in kwargs.items():
+#         person_info[key] = value
+# 
+#     result = f"This is {person_info['name']} from {person_info['town']} and he is {person_info['age']} years old"
+#     return result
+# 
+# 
+# print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+
+
+
+# def get_info(name, age, town):
+#     return f"This is {name} from {town} and he is {age} years old"
+#
+# print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
