c771cbafd52e6d4dae57ce30f79025daff613e67
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-10 20:39:43 +0200

    Update 1_multiplication_function.py

--- Functions Advanced - Lab/1_multiplication_function.py
+++ Functions Advanced - Lab/1_multiplication_function.py
@@ -1,10 +1,23 @@
+from functools import reduce
+
+
 def multiply(*args):
-    result = 1
-    for item in args:
-        result *= item
-    return result
+    return reduce(lambda x, y: x*y, args)
 
 
 print(multiply(1, 4, 5))
 print(multiply(4, 5, 6, 1, 3))
 print(multiply(2, 0, 1000, 5000))
+
+
+
+# def multiply(*args):
+#     result = 1
+#     for item in args:
+#         result *= item
+#     return result
+
+
+# print(multiply(1, 4, 5))
+# print(multiply(4, 5, 6, 1, 3))
+# print(multiply(2, 0, 1000, 5000))
