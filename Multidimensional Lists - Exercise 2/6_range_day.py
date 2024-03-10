3767f15966d897c891ef4a7a4668e1d16743bd2f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-10 17:51:02 +0200

    Update 6_range_day.py

--- Multidimensional Lists - Exercise 2/6_range_day.py
+++ Multidimensional Lists - Exercise 2/6_range_day.py
@@ -1,72 +1,99 @@
-def move(direction, steps, s_row, s_col):
-    new_row, new_col = 0, 0
-    if direction == "up":
-        new_row, new_col = s_row - steps, s_col
-    elif direction == "down":
-        new_row, new_col = s_row + steps, s_col
-    elif direction == "left":
-        new_row, new_col = s_row, s_col - steps
-    elif direction == "right":
-        new_row, new_col = s_row, s_col + steps
-    if new_row in range(size) and new_col in range(size) and field[new_row][new_col] == ".":
-        field[s_row][s_col] = "."
-        s_row, s_col = new_row, new_col
-        field[s_row][s_col] = "A"
-    return s_row, s_col
+def fill_the_matrix_and_find_shooter_and_targets(s_row, s_col, mtrx_size):
+    num_targets = 0
+    matrix = []
+    for r in range(mtrx_size):
+        row = input().split()
+        matrix.append(row)
+        if "A" in row:
+            s_row = r
+            s_col = row.index('A')
+        if "x" in row:
+            num_targets += row.count("x")
+    return matrix, s_row, s_col, num_targets
 
 
-def shoot(direction, s_row, s_col):
+def is_valid_index(value, max_value):
+    return 0 <= value < max_value
+
+
+def next_move(current_direction, curr_row, curr_col, mtrx_size, steps_number):
+    if current_direction == "up" and is_valid_index(curr_row - steps_number, mtrx_size):
+        return curr_row - steps_number, curr_col
+    if current_direction == "down" and is_valid_index(curr_row + steps_number, mtrx_size):
+        return curr_row + steps_number, curr_col
+    if current_direction == "left" and is_valid_index(curr_col - steps_number, mtrx_size):
+        return curr_row, curr_col - steps_number
+    if current_direction == "right" and is_valid_index(curr_col + steps_number, mtrx_size):
+        return curr_row, curr_col + steps_number
+    return None, None
+
+
+def shoot(matrix, current_direction, s_row, s_col, mtrx_size):
     possible_targets = []
-    if direction == "up":
-        possible_targets.extend([[row, s_col] for row in range(s_row - 1, - 1, - 1)])
-    elif direction == "down":
-        possible_targets.extend([[row, s_col] for row in range(s_row + 1, size)])
-    elif direction == "left":
-        possible_targets.extend([[s_row, col] for col in range(s_col - 1, - 1, - 1)])
-    elif direction == "right":
-        possible_targets.extend([[s_row, col] for col in range(s_col + 1, size)])
+    if current_direction == "up":
+        for row in range(s_row - 1, -1, -1):
+            possible_targets.append([row, s_col])
+    elif current_direction == "down":
+        for row in range(s_row + 1, mtrx_size):
+            possible_targets.append([row, s_col])
+    elif current_direction == "left":
+        for col in range(s_col - 1, -1, -1):
+            possible_targets.append([s_row, col])
+    elif current_direction == "right":
+        for col in range(s_col + 1, mtrx_size):
+            possible_targets.append([s_row, col])
     for target in possible_targets:
         target_row, target_col = target
-        if target_row in range(size) and target_col in range(size) and field[target_row][target_col] == "x":
-            field[target_row][target_col] = "."
-            return [target_row, target_col]
-    return []
+        if matrix[target_row][target_col] == "x":
+            matrix[target_row][target_col] = "."
+            return matrix, [target_row, target_col]
+    return matrix, []
 
 
-size = 5
-field = []
-shooter_row, shooter_col = 0, 0
-targets = 0
+def main():
+    size = 5
+    field = []
+    shooter_row, shooter_col = None, None
+    targets_number_left = 0
 
-for row in range(size):
-    current_row = [x for x in input().split()]
-    field.append(current_row)
-    for symbol in current_row:
-        if symbol == "A":
-            shooter_row, shooter_col = row, current_row.index("A")
-        elif symbol == "x":
-            targets += 1
+    field, shooter_row, shooter_col, targets_number_left = fill_the_matrix_and_find_shooter_and_targets(shooter_row, shooter_col, size)
+    number_of_commands = int(input())
+    shot_targets_coordinates = []
 
-command_count = int(input())
-shot_targets = []
+    for _ in range(number_of_commands):
+        data = input().split()
+        command, direction = data[0], data[1]
 
-for _ in range(command_count):
-    command = input().split()
-    if command[0] == "move":
-        move_direction, steps = command[1], int(command[2])
-        shooter_row, shooter_col = move(move_direction, steps, shooter_row, shooter_col)
-    elif command[0] == "shoot":
-        shoot_direction = command[1]
-        coordinates = shoot(shoot_direction, shooter_row, shooter_col)
-        if coordinates:
-            shot_targets.append(coordinates)
-            targets -= 1
-    if targets == 0:
-        break
+        if command == "shoot":
+            field, coordinates = shoot(field, direction, shooter_row, shooter_col, size)
+            if coordinates:
+                shot_targets_coordinates.append(coordinates)
+                targets_number_left -= 1
+                if targets_number_left == 0:
+                    break
 
-if targets == 0:
-    print(f"Training completed! All {len(shot_targets)} targets hit.")
-else:
-    print(f"Training not completed! {targets} targets left.")
+        elif command == "move":
+            steps = int(data[2])
+            next_row, next_col = next_move(direction, shooter_row, shooter_col, size, steps)
 
-[print(element) for element in shot_targets]
+            if next_row is None or next_col is None:
+                continue
+
+            if field[next_row][next_col] == "x":
+                continue
+
+            field[shooter_row][shooter_col] = "."
+            shooter_row, shooter_col = next_row, next_col
+            field[shooter_row][shooter_col] = "A"  # to check where is shooter
+
+    if targets_number_left == 0:
+        targets_left = len(shot_targets_coordinates)
+        print(f"Training completed! All {targets_left} targets hit.")
+    else:
+        print(f"Training not completed! {targets_number_left} targets left.")
+
+    for curr_row in shot_targets_coordinates:
+        print(curr_row)
+
+
+main()
