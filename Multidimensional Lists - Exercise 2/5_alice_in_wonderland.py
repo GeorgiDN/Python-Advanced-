e889d9d0da0849c11211b9255109ed64e2ee642d
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-10 16:23:22 +0200

    Update 5_alice_in_wonderland.py

--- Multidimensional Lists - Exercise 2/5_alice_in_wonderland.py
+++ Multidimensional Lists - Exercise 2/5_alice_in_wonderland.py
@@ -1,48 +1,77 @@
-size = int(input())
-wonderland = []
-alice_row, alice_col = 0, 0
+def fill_the_matrix_and_find_alice(a_row, a_col, mtrx_size):
+    matrix = []
+    for r in range(mtrx_size):
+        row = input().split()
+        matrix.append(row)
+        if "A" in row:
+            a_row = r
+            a_col = row.index('A')
+    return matrix, a_row, a_col
 
-for row in range(size):
-    current_row = [x for x in input().split()]
-    wonderland.append(current_row)
-    if "A" in current_row:
-        alice_row, alice_col = row, current_row.index("A")
 
-alice_left_wonderland = False
-alice_got_to_the_party = False
-tea_bags = 0
+def is_valid_index(value, max_value):
+    return 0 <= value < max_value
 
-while True:
-    wonderland[alice_row][alice_col] = "*"
-    if alice_left_wonderland or alice_got_to_the_party:
-        break
-    direction = input()
-    if direction == "up":
-        next_row, next_col = alice_row - 1, alice_col
-    elif direction == "down":
-        next_row, next_col = alice_row + 1, alice_col
-    elif direction == "left":
-        next_row, next_col = alice_row, alice_col - 1
-    else:
-        next_row, next_col = alice_row, alice_col + 1
 
-    if next_row in range(size) and next_col in range(size):
-        symbol = wonderland[next_row][next_col]
-        if symbol == "R":
-            wonderland[next_row][next_col] = "*"
-            alice_left_wonderland = True
+def next_move(current_direction, curr_row, curr_col, mtrx_size):
+    if current_direction == "up" and is_valid_index(curr_row - 1, mtrx_size):
+        return curr_row - 1, curr_col
+    if current_direction == "down" and is_valid_index(curr_row + 1, mtrx_size):
+        return curr_row + 1, curr_col
+    if current_direction == "left" and is_valid_index(curr_col - 1, mtrx_size):
+        return curr_row, curr_col - 1
+    if current_direction == "right" and is_valid_index(curr_col + 1, mtrx_size):
+        return curr_row, curr_col + 1
+    return None, None
+
+
+def main():
+    size = int(input())
+    field = []
+    alice_row, alice_col = None, None
+    collected_bags = 0
+    alice_go_to_party = False
+    alice_left_wonderland = False
+
+    field, alice_row, alice_col = fill_the_matrix_and_find_alice(alice_row, alice_col, size)
+    direction = " "
+    while direction:
+        if alice_left_wonderland or alice_go_to_party:
             break
-        elif symbol.isalnum():
-            tea_bags += int(symbol)
+        direction = input()
+        next_row, next_col = next_move(direction, alice_row, alice_col, size)
+        if next_row is None or next_col is None:
+            alice_left_wonderland = True
+            field[alice_row][alice_col] = "*"
+            break
+
+        if field[next_row][next_col] == "R":
+            alice_left_wonderland = True
+            field[alice_row][alice_col] = "*"
+            alice_row, alice_col = next_row, next_col
+            field[alice_row][alice_col] = "A"  # to check where is alice
+            field[alice_row][alice_col] = "*"
+            break
+
+        if field[next_row][next_col].isdigit():
+            bags_of_tea = int(field[next_row][next_col])
+            collected_bags += bags_of_tea
+
+        field[alice_row][alice_col] = "*"
         alice_row, alice_col = next_row, next_col
-        if tea_bags >= 10:
-            alice_got_to_the_party = True
-    else:
-        alice_left_wonderland = True
+        field[alice_row][alice_col] = "A"  # to check where is alice
+        field[alice_row][alice_col] = "*"
+        if collected_bags >= 10:
+            alice_go_to_party = True
 
-if alice_got_to_the_party:
-    print("She did it! She went to the party.")
-if alice_left_wonderland:
-    print("Alice didn't make it to the tea party.")
-[print(" ".join(element)) for element in wonderland]
+    if alice_go_to_party:
+        print("She did it! She went to the party.")
+    elif alice_left_wonderland:
+        print("Alice didn't make it to the tea party.")
 
+    [print(' '.join(curr_row)) for curr_row in field]
+    # for curr_row in field:
+    #     print(' '.join(curr_row))
+
+
+main()
