55759907c7355787d6a49d6f3305bd8c37325b92
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-13 22:23:12 +0200

    Update 4_number_filter.py

--- Functions Advanced - Exercise/4_number_filter.py
+++ Functions Advanced - Exercise/4_number_filter.py
@@ -1,24 +1,46 @@
 def even_odd_filter(**kwargs):
-    result = {}
+    my_dict = {}
+    for kind, nums in kwargs.items():
+        my_dict[kind] = []
+        for num in nums:
+            if kind == "even" and num % 2 == 0:
+                my_dict["even"].append(num)
+            elif kind == "odd" and num % 2 != 0:
+                my_dict["odd"].append(num)
 
-    for key in kwargs:
-        if key == "odd":
-            result[key] = [x for x in kwargs[key] if x % 2 != 0]
-        elif key == "even":
-            result[key] = [x for x in kwargs[key] if x % 2 == 0]
+    sorted_dict = dict(sorted(my_dict.items(), key=lambda x: -len(x[1])))
+    return sorted_dict
 
-    return dict(sorted(result.items(), key=lambda x: -len(x[1])))
-
-
-
-# print(even_odd_filter(
-#     odd=[2, 2, 30, 44, 10, 5],
-# ))
 
 print(even_odd_filter(
     odd=[1, 2, 3, 4, 10, 5],
     even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
 ))
+
+
+
+# def even_odd_filter(**kwargs):
+#     result = {}
+
+#     for key in kwargs:
+#         if key == "odd":
+#             result[key] = [x for x in kwargs[key] if x % 2 != 0]
+#         elif key == "even":
+#             result[key] = [x for x in kwargs[key] if x % 2 == 0]
+
+#     return dict(sorted(result.items(), key=lambda x: -len(x[1])))
+
+
+
+# # print(even_odd_filter(
+# #     odd=[2, 2, 30, 44, 10, 5],
+# # ))
+
+# print(even_odd_filter(
+#     odd=[1, 2, 3, 4, 10, 5],
+#     even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+# ))
+
 
 
 
