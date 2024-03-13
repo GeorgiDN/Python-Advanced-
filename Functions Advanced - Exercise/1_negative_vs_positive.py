a9b226aae9105394d010a63db475791da19d0e57
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-13 21:20:05 +0200

    Update 1_negative_vs_positive.py

--- Functions Advanced - Exercise/1_negative_vs_positive.py
+++ Functions Advanced - Exercise/1_negative_vs_positive.py
@@ -1,20 +1,50 @@
-def sort_numbers():
-    [negatives.append(x) for x in numbers if x < 0]
-    [positives.append(x) for x in numbers if x >= 0]
+def sum_positive_numbers(nums):
+    return sum([n for n in nums if n > 0])
 
 
-numbers = [int(x) for x in input().split()]
-negatives, positives = [], []
+def sum_negative_numbers(nums):
+    return sum([n for n in nums if n < 0])
 
-sort_numbers()
 
-print(sum(negatives))
-print(sum(positives))
+def main():
+    list_with_numbers = [int(x) for x in input().split()]
+    result = ''
+    positive_sum = sum_positive_numbers(list_with_numbers)
+    negative_sum = sum_negative_numbers(list_with_numbers)
 
-if abs(sum(negatives)) > sum(positives):
-    print("The negatives are stronger than the positives")
-else:
-    print("The positives are stronger than the negatives")
+    result += f"{negative_sum}\n"
+    result += f"{positive_sum}\n"
+
+    if abs(positive_sum) < abs(negative_sum):
+        result += "The negatives are stronger than the positives"
+    else:
+        result += "The positives are stronger than the negatives"
+
+    return print(result)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+# def sort_numbers():
+#     [negatives.append(x) for x in numbers if x < 0]
+#     [positives.append(x) for x in numbers if x >= 0]
+
+
+# numbers = [int(x) for x in input().split()]
+# negatives, positives = [], []
+
+# sort_numbers()
+
+# print(sum(negatives))
+# print(sum(positives))
+
+# if abs(sum(negatives)) > sum(positives):
+#     print("The negatives are stronger than the positives")
+# else:
+#     print("The positives are stronger than the negatives")
 
 
 
