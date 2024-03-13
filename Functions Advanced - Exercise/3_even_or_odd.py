cc7fade48cb1bd023986952ff56a154a4d0db2ce
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-13 21:59:01 +0200

    Update 3_even_or_odd.py

--- Functions Advanced - Exercise/3_even_or_odd.py
+++ Functions Advanced - Exercise/3_even_or_odd.py
@@ -1,14 +1,27 @@
 def even_odd(*args):
-    *numbers, command = args
-    result = []
+    nums, command = list(map(int, args[:-1])), args[-1]
+    if command == "even":
+        return [n for n in nums if n % 2 == 0]
+    return [n for n in nums if n % 2 != 0]
 
-    if command == "odd":
-        [result.append(x) for x in numbers if x % 2 != 0]
-
-    elif command == "even":
-        [result.append(x) for x in numbers if x % 2 == 0]
-
-    return result
 
 print(even_odd(1, 2, 3, 4, 5, 6, "even"))
 print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
+
+
+
+
+# def even_odd(*args):
+#     *numbers, command = args
+#     result = []
+
+#     if command == "odd":
+#         [result.append(x) for x in numbers if x % 2 != 0]
+
+#     elif command == "even":
+#         [result.append(x) for x in numbers if x % 2 == 0]
+
+#     return result
+
+# print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+# print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
