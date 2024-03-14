a08ea072f00f49df66242154c431f7c023a283ff
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-14 20:56:14 +0200

    Update 9_recursion_palindrome.py

--- Functions Advanced - Exercise/9_recursion_palindrome.py
+++ Functions Advanced - Exercise/9_recursion_palindrome.py
@@ -1,25 +1,13 @@
 def palindrome(word, idx):
-    if word == word[::-1]:
+    if idx == len(word) // 2:
         return f"{word} is a palindrome"
+
+    if word[idx] == word[-idx - 1]:
+        return palindrome(word, idx + 1)
+
     else:
         return f"{word} is not a palindrome"
 
+
 print(palindrome("abcba", 0))
-
 print(palindrome("peter", 0))
-
-
-
-
-# def palindrome(word, idx):
-#     if idx == len(word) // 2:
-#         return f"{word} is a palindrome"
-
-#     if word[idx] == word[-idx - 1]:
-#         return palindrome(word, idx + 1)
-
-#     else:
-#         return f"{word} is not a palindrome"
-
-# print(palindrome("abcba", 0))
-
