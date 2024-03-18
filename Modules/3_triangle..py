876d21192b936d10ee5ca654671175329a34e567
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-03-18 19:05:21 +0200

    Delete Modules directory

--- Modules/3_triangle..py
+++
@@ -1,4 +0,0 @@
-from python_advanced.modules_lab.triangle_size_modules_1 import print_triangle
-
-size = int(input())
-print_triangle(size)
--- Modules/4_mathematical_operations.py
+++
@@ -1,8 +0,0 @@
-from python_advanced.modules_lab.basic_calculations import calculator
-
-inp = input().split()
-number1 = float(inp[0])
-sign = inp[1]
-number2 = int(inp[2])
-
-print(calculator(number1, sign, number2))
