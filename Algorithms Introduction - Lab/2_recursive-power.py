5ec8bf7f9c820f5ec9a2ffc51f28bf602223fa6e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-05-02 20:04:09 +0300

    Update 2_recursive-power.py

--- Algorithms Introduction - Lab/2_recursive-power.py
+++ Algorithms Introduction - Lab/2_recursive-power.py
@@ -1,9 +1,28 @@
-def recursive_power(n):
+def recursive_factorial(n):
     if n == 1:
         return n
 
-    return n * recursive_power(n-1)
+    return n * recursive_factorial(n-1)
 
 
 num = int(input())
-print(recursive_power(num))
+print(recursive_factorial(num))
+
+
+
+# from functools import reduce
+# num = int(input())
+# my_list = list(range(1, num + 1))
+# res = reduce(lambda x, y: x*y, my_list)
+# print(res)
+
+
+
+# EASY WAY
+# num = int(input())
+# res = 1
+#
+# for n in range(num, 0, -1):
+#     res *= n
+#
+# print(res)
