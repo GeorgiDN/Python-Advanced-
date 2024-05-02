d4d72dd2c435d06c133b15a507905de91da99077
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-05-02 22:13:13 +0300

    add solution

---
+++ Algorithms Introduction - Lab/4_sum_of_coins.py
@@ -0,0 +1,30 @@
+def sum_of_coins(coins, target):
+    coins = sorted(coins, reverse=True)
+    idx = 0
+    used_coins = {}
+
+    while True:
+        if target <= 0 or idx >= len(coins):
+            break
+        count_coins = target // coins[idx]
+        target = target % coins[idx]
+
+        if count_coins > 0:
+            used_coins[coins[idx]] = count_coins
+
+        idx += 1
+
+    if target != 0:
+        return 'Error'
+    else:
+        result = f'Number of coins to take: {sum(used_coins.values())}\n'
+
+        for coin, count in used_coins.items():
+            result += f'{count} coin(s) with value {coin}\n'
+
+        return result
+
+
+coins = list(map(int, input().split(', ')))
+target = int(input())
+print(sum_of_coins(coins, target))
