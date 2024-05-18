9dfdd7ea78d37a50f6f237207511de0cd023c0f6
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-05-18 20:01:03 +0300

    added_solution

--- Lists as Stacks and Queues - Exercise/6_balanced_paretheses_.py
+++ Lists as Stacks and Queues - Exercise/6_balanced_paretheses_.py
@@ -4,15 +4,13 @@ my_stack = []
 is_balanced = True
 valid_parentheses = {"(": ")", "{": "}", "[": "]"}
 
+
 for bracket in parenthesis:
-    if bracket in "([{":
+    if bracket in "({[":
         my_stack.append(bracket)
     else:
         if not my_stack or bracket != valid_parentheses[my_stack.pop()]:
             is_balanced = False
             break
 
-if is_balanced:
-    print("YES")
-else:
-    print("NO")
+print("YES") if is_balanced and len(my_stack) % 2 == 0 else print("NO")
