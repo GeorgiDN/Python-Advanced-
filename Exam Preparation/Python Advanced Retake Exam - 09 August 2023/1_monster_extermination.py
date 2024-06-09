bf7014ecdd410ed16655babf553776e23ee78240
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-09 20:20:28 +0300

    Update 1_monster_extermination.py

--- Exam Preparation/Python Advanced Retake Exam - 09 August 2023/1_monster_extermination.py
+++ Exam Preparation/Python Advanced Retake Exam - 09 August 2023/1_monster_extermination.py
@@ -1,31 +1,29 @@
-#https://judge.softuni.org/Contests/Practice/Index/4089#0
+# #https://judge.softuni.org/Contests/Practice/Index/4089#0
 
 from collections import deque
 
-armour_values = deque(map(int, input().split(",")))
-soldier_strike_impacts = list(map(int, input().split(",")))
-monster_killed = 0
+monsters = deque(map(int, input().split(',')))
+soldiers = list(map(int, input().split(',')))
+monsters_killed = 0
 
-while armour_values and soldier_strike_impacts:
-    monster_armour = armour_values.popleft()
-    soldier_strike = soldier_strike_impacts.pop()
-    if soldier_strike == monster_armour:
-        monster_killed += 1
-    elif soldier_strike > monster_armour:
-        monster_killed += 1
-        rest_impact = soldier_strike - monster_armour
-        if len(soldier_strike_impacts) > 0:
-            soldier_strike_impacts[-1] += rest_impact
-        elif not soldier_strike_impacts and rest_impact > 0:
-            soldier_strike_impacts.append(rest_impact)
-    elif soldier_strike < monster_armour:
-        monster_armour -= soldier_strike
-        armour_values.append(monster_armour)
+while monsters and soldiers:
+    monster = monsters.popleft()
+    strike = soldiers.pop()
 
+    if strike >= monster:
+        strike -= monster
+        monsters_killed += 1
+        if strike > 0:
+            if not soldiers:
+                soldiers.append(strike)
+            else:
+                soldiers[-1] += strike
+    else:
+        monster -= strike
+        monsters.append(monster)
 
-if not armour_values:
+if not monsters:
     print("All monsters have been killed!")
-if not soldier_strike_impacts:
+if not soldiers:
     print("The soldier has been defeated.")
-
-print(f"Total monsters killed: {monster_killed}")
+print(f"Total monsters killed: {monsters_killed}")
