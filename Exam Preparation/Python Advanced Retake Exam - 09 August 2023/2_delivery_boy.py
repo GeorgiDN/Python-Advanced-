c47b4dfe5a59d08254d9bd9dfd3700bb8a2b0511
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-09 18:51:42 +0300

    Update 2_delivery_boy.py

--- Exam Preparation/Python Advanced Retake Exam - 09 August 2023/2_delivery_boy.py
+++ Exam Preparation/Python Advanced Retake Exam - 09 August 2023/2_delivery_boy.py
@@ -1,60 +1,82 @@
-def is_valid(value, max_value):
-    return 0 <= value < max_value
+def is_valid_index(idx, size):
+    return 0 <= idx < size
 
 
-def next_move(command, current_row, current_col):
-    if command == 'up' and is_valid(current_row-1, rows):
-        return current_row-1, current_col
-    if command == 'down' and is_valid(current_row+1, rows):
-        return current_row+1, current_col
-    if command == 'left' and is_valid(current_col-1, cols):
-        return current_row, current_col-1
-    if command == 'right' and is_valid(current_col+1, cols):
-        return current_row, current_col+1
+def next_move(rows, cols, row, col, direction):
+    all_directions = {"up": [row - 1, col],
+                      "down": [row + 1, col],
+                      "right": [row, col + 1],
+                      "left": [row, col - 1]}
+
+    new_row, new_col = all_directions[direction][0], all_directions[direction][1]
+    if is_valid_index(new_row, rows) \
+            and is_valid_index(new_col, cols):
+        return new_row, new_col
     return None, None
 
 
-rows, cols = [int(x) for x in input().split(' ')]
-field = []
-start_row, start_col = None, None
-boy_row, boy_col = None, None
-line = ' '
+def fill_the_matrix_and_take_positions(rows, b_row, b_col, field):
+    field = []
+    for r in range(rows):
+        row = list(input())
+        field.append(row)
+        if "B" in row:
+            b_row = r
+            b_col = row.index("B")
 
-for r in range(rows):
-    row = list(input())
-    field.append(row)
-    if 'B' in row:
-        boy_row = r
-        boy_col = row.index('B')
-        start_row = boy_row
-        start_col = boy_col
+    return field, b_row, b_col
 
-while line:
-    line = input()
-    next_row, next_col = next_move(line, boy_row, boy_col)
-    if next_row is None or next_col is None:
-        print('The delivery is late. Order is canceled.')
-        field[start_row][start_col] = ' '
-        break
-    if field[next_row][next_col] == '*':
-        continue
-    if field[next_row][next_col] == 'A':
-        field[boy_row][boy_col] = '.'
+
+def check_out_of_boundaries(next_row, next_col):
+    return next_row is None or next_col is None
+
+
+def main():
+    field = []
+    boy_row, boy_col = 0, 0
+    rows, cols = [int(x) for x in input().split()]
+    field, boy_row, boy_col = fill_the_matrix_and_take_positions(rows, boy_row, boy_col, field)
+    out_of_field = False
+    start_row, start_col = boy_row, boy_col
+
+    while True:
+        direction = input()
+        if not direction:
+            break
+
+        next_row, next_col = next_move(rows, cols, boy_row, boy_col, direction)
+        out_of_field = check_out_of_boundaries(next_row, next_col)
+        if out_of_field:
+            field[start_row][start_col] = ' '
+            print('The delivery is late. Order is canceled.')
+            break
+
+        if field[next_row][next_col] == "*":
+            continue
+
+        if field[next_row][next_col] == "P":
+            print('Pizza is collected. 10 minutes for delivery.')
+            field[boy_row][boy_col] = '.'
+            boy_row, boy_col = next_row, next_col
+            field[next_row][next_col] = "R"
+            continue
+
+        if field[next_row][next_col] == "A":
+            print("Pizza is delivered on time! Next order...")
+            field[next_row][next_col] = "P"
+            if field[boy_row][boy_col] != "R":
+                field[boy_row][boy_col] = '.'
+            field[start_row][start_col] = 'B'
+            break
+
+        if field[boy_row][boy_col] != "R":
+            field[boy_row][boy_col] = "B"
+            field[boy_row][boy_col] = '.'
         boy_row, boy_col = next_row, next_col
-        field[boy_row][boy_col] = 'P'
-        print("Pizza is delivered on time! Next order...")
-        field[start_row][start_col] = 'B'
-        break
-    if field[next_row][next_col] == 'P':
         field[boy_row][boy_col] = '.'
-        boy_row, boy_col = next_row, next_col
-        field[next_row][next_col] = 'R'
-        print("Pizza is collected. 10 minutes for delivery.")
-        continue
-    if not field[boy_row][boy_col] == 'R':
-        field[boy_row][boy_col] = '.'
-    boy_row, boy_col = next_row, next_col
-    field[boy_row][boy_col] = '.'
 
-for row in field:
-    print(''.join(row))
+    for r in field:
+        print(''.join(r))
+
+
+main()
