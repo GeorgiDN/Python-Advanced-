08c8528d23b30aa371bd3b7478b3f3896f2c8448
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-13 09:47:20 +0300

    Update 3_team_lineup.py

--- Exam Preparation/Python Advanced Exam - 22 October 2023/3_team_lineup.py
+++ Exam Preparation/Python Advanced Exam - 22 October 2023/3_team_lineup.py
@@ -3,23 +3,43 @@ def team_lineup(*args):
 
     for player, country in args:
         if country not in football_information:
-            football_information[country] = [player]
-        else:
-            football_information[country].append(player)
+            football_information[country] = []
+        football_information[country].append(player)
 
-    sort_information = dict(sorted(football_information.items(), key=lambda x: (-len(x[1]), x[0])))
+    sort_football_information = dict(sorted(football_information.items(), key=lambda x: (-len(x[1]), x[0])))
 
-    final_result = ''
-    for k, v in sort_information.items():
-       final_result += f"{k}:\n"
-       final_result += "  -" + f"\n  -".join(v) + "\n"
+    result = ''
+    for country, player_name in sort_football_information.items():
+        result += f"{country}:\n"
+        result += "  -" + f"\n  -".join(player_name) + "\n"
 
-    return final_result
+    return result.strip()
 
 
-print(team_lineup(("Harry Kane", "England"),
-                  ("Manuel Neuer", "Germany"),
-                  ("Raheem Sterling", "England"),
-                  ("Toni Kroos", "Germany"),
-                  ("Cristiano Ronaldo", "Portugal"),
-                  ("Thomas Muller", "Germany")))
+# print(team_lineup(("Harry Kane", "England"),
+#                   ("Manuel Neuer", "Germany"),
+#                   ("Raheem Sterling", "England"),
+#                   ("Toni Kroos", "Germany"),
+#                   ("Cristiano Ronaldo", "Portugal"),
+#                   ("Thomas Muller", "Germany")))
+
+
+# print(team_lineup(
+#    ("Lionel Messi", "Argentina"),
+#    ("Neymar", "Brazil"),
+#    ("Cristiano Ronaldo", "Portugal"),
+#    ("Harry Kane", "England"),
+#    ("Kylian Mbappe", "France"),
+#    ("Raheem Sterling", "England")))
+
+
+# print(team_lineup(
+#    ("Harry Kane", "England"),
+#    ("Manuel Neuer", "Germany"),
+#    ("Raheem Sterling", "England"),
+#    ("Toni Kroos", "Germany"),
+#    ("Cristiano Ronaldo", "Portugal"),
+#    ("Thomas Muller", "Germany"),
+#    ("Bruno Fernandes", "Portugal"),
+#    ("Bernardo Silva", "Portugal"),
+#    ("Harry Maguire", "England")))
