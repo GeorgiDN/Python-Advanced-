39d21106db2df5b90367cce7493a6635edc04e3b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-16 10:08:37 +0300

    Update 3_softuni_students.py

--- Exam Preparation/Python Advanced Retake Exam - 13 December/3_softuni_students.py
+++ Exam Preparation/Python Advanced Retake Exam - 13 December/3_softuni_students.py
@@ -3,35 +3,55 @@ def softuni_students(*args, **kwargs):
     valid_students = {}
     not_valid_students = []
 
-    for key, val in kwargs.items():
-        valid_courses[key] = val
+    for _id, course in kwargs.items():
+        valid_courses[_id] = course
 
-    for info in args:
-        id_ = info[0]
-        name = info[1]
-        if id_ in valid_courses:
-            valid_students[name] = valid_courses[id_]
+    for data in args:
+        course_id = data[0]
+        username = data[1]
+        if course_id in valid_courses:
+            valid_students[username] = valid_courses[course_id]
         else:
-            not_valid_students.append(name)
+            not_valid_students.append(username)
 
     if not_valid_students:
         sorted_not_valid_students = sorted(not_valid_students)
     sorted_valid_students = dict(sorted(valid_students.items()))
 
-    final_result = ''
-    for k, v in sorted_valid_students.items():
-        final_result += f"*** A student with the username {k} has successfully finished the course {v}!\n"
+    result = ''
+    for username, course in sorted_valid_students.items():
+        result += f"*** A student with the username {username} has successfully finished the course {course}!\n"
 
     if not_valid_students:
-        final_result += f"!!! Invalid course students: {', '.join(sorted_not_valid_students)}"
+        result += f"!!! Invalid course students: {', '.join(sorted_not_valid_students)}"
 
-    return final_result
+    return result
 
 
-print(softuni_students(
-    ('id_7', 'Silvester1'),
-    ('id_32', 'Katq21'),
-    ('id_7', 'The programmer'),
-    id_76='Spring Fundamentals',
-    id_7='Spring Advanced',
-))
+# print(softuni_students(
+#     ('id_7', 'Silvester1'),
+#     ('id_32', 'Katq21'),
+#     ('id_7', 'The programmer'),
+#     id_76='Spring Fundamentals',
+#     id_7='Spring Advanced',
+# ))
+
+
+
+# print(softuni_students(
+#     ('id_1', 'Kaloyan9905'),
+#     id_1='Python Web Framework',
+# ))
+
+
+
+# print(softuni_students(
+#     ('id_22', 'Programmingkitten'),
+#     ('id_11', 'MitkoTheDark'),
+#     ('id_321', 'Bobosa253'),
+#     ('id_08', 'KrasimirAtanasov'),
+#     ('id_32', 'DaniBG'),
+#     id_321='HTML & CSS',
+#     id_22='Machine Learning',
+#     id_08='JS Advanced',
+# ))
