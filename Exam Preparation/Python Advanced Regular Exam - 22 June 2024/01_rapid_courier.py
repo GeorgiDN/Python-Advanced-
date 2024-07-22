439eea54b35fc09a273e79cf4ea84c55829bfb3d
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-22 18:53:42 +0300

    Update 01_rapid_courier.py

--- Exam Preparation/Python Advanced Regular Exam - 22 June 2024/01_rapid_courier.py
+++ Exam Preparation/Python Advanced Regular Exam - 22 June 2024/01_rapid_courier.py
@@ -1,33 +1,34 @@
 from collections import deque
 
-packages = list(map(int, input().split(' ')))
-couriers = deque(map(int, input().split(' ')))
+packages = list(map(int, input().split()))
+couriers = deque(map(int, input().split()))
 total_weight = 0
 
 while packages and couriers:
-    courier = couriers.popleft()
     package = packages.pop()
+    courier_capacity = couriers.popleft()
 
-    if courier >= package:
-        if courier > package:
-            courier -= package * 2
-            if courier > 0:
-                couriers.append(courier)
+    if courier_capacity >= package:
+        if courier_capacity > package:
+            courier_capacity -= (package * 2)
+            if courier_capacity > 0:
+                couriers.append(courier_capacity)
         total_weight += package
-
     else:
-        package -= courier
+        package -= courier_capacity
         packages.append(package)
-        total_weight += courier
+        total_weight += courier_capacity
 
 print(f"Total weight: {total_weight} kg")
-
 if not packages and not couriers:
-    print(f"Congratulations, all packages were delivered successfully by the couriers today.")
+    print("Congratulations, all packages were delivered successfully by the couriers today.")
 elif packages and not couriers:
-    print(f"Unfortunately, there are no more available couriers to deliver the following packages: {', '.join(map(str, packages))}")
-elif couriers and not packages:
-    print(f"Couriers are still on duty: {', '.join(map(str, couriers))} but there are no more packages to deliver.")
+    sting_packages = ", ".join(map(str, packages))
+    print(f"Unfortunately, there are no more available couriers to deliver the following packages: {sting_packages}")
+elif not packages and couriers:
+    sting_couriers = ", ".join(map(str, couriers))
+    print(f"Couriers are still on duty: {sting_couriers} but there are no more packages to deliver.")
+
 
 
 
