0fdba9eabe6fd2aa55782a6dad70668a0b4256b8
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-22 19:38:25 +0300

    Update 03_boarding_passengers.py

--- Exam Preparation/Python Advanced Regular Exam - 22 June 2024/03_boarding_passengers.py
+++ Exam Preparation/Python Advanced Regular Exam - 22 June 2024/03_boarding_passengers.py
@@ -1,44 +1,81 @@
 def boarding_passengers(*args):
-
     capacity_of_ship = int(args[0])
-    rest = args[1:]
-    passengers_left = len(rest)
-    passengers_on_board = {}
+    passenger_groups = args[1:]
     all_on_the_board = True
+    board = {}
 
-    for number_of_passengers, benefit_name in rest:
+    for data in passenger_groups:
+        number_passengers, program_name = int(data[0]), data[1]
         if capacity_of_ship == 0:
+            all_on_the_board = False
             break
-
-        if capacity_of_ship >= number_of_passengers:
-            if benefit_name not in passengers_on_board:
-                passengers_on_board[benefit_name] = 0
-            passengers_on_board[benefit_name] += number_of_passengers
-            passengers_left -= 1
-            capacity_of_ship -= number_of_passengers
+        if number_passengers <= capacity_of_ship:
+            if program_name not in board:
+                board[program_name] = 0
+            board[program_name] += number_passengers
+            capacity_of_ship -= number_passengers
         else:
             all_on_the_board = False
 
-    sorted_data = sorted(passengers_on_board.items(), key=lambda x: (-x[1], (x[0])))
+    sorted_board = dict(sorted(board.items(), key=lambda x: (-x[1], x[0])))
+    result = "Boarding details by benefit plan:\n"
 
-    result = ''
-    result += 'Boarding details by benefit plan:\n'
+    for benefit_plan, total_number_of_passengers in sorted_board.items():
+        result += f"## {benefit_plan}: {total_number_of_passengers} guests\n"
 
-    for plan, count in sorted_data:
-        result += f'## {plan}: {count} guests\n'
-
-    if passengers_left == 0 and all_on_the_board:
-        result += "All passengers are successfully boarded!"
-
-    elif capacity_of_ship == 0 and passengers_left > 0:
-        result += "Boarding unsuccessful. Cruise ship at full capacity."
-
+    if all_on_the_board:
+        result += "All passengers are successfully boarded!\n"
+    elif capacity_of_ship == 0 and not all_on_the_board:
+        result += "Boarding unsuccessful. Cruise ship at full capacity.\n"
     elif capacity_of_ship > 0 and not all_on_the_board:
-        result += f"Partial boarding completed. Available capacity: {capacity_of_ship}."
+        result += f"Partial boarding completed. Available capacity: {capacity_of_ship}.\n"
 
-    return result
+    return result.strip()
 
 
 # print(boarding_passengers(150, (35, 'Diamond'), (55, 'Platinum'), (35, 'Gold'), (25, 'First Cruiser')))
-#print(boarding_passengers(100, (20, 'Diamond'), (15, 'Platinum'), (25, 'Gold'), (25, 'First Cruiser'), (15, 'Diamond'), (10, 'Gold')))
-#print(boarding_passengers(120, (30, 'Gold'), (20, 'Platinum'), (30, 'Diamond'), (10, 'First Cruiser'), (31, 'Platinum'), (20, 'Diamond')))
+# print(boarding_passengers(100, (20, 'Diamond'), (15, 'Platinum'), (25, 'Gold'), (25, 'First Cruiser'), (15, 'Diamond'), (10, 'Gold')))
+# print(boarding_passengers(120, (30, 'Gold'), (20, 'Platinum'), (30, 'Diamond'), (10, 'First Cruiser'), (31, 'Platinum'), (20, 'Diamond')))
+
+
+
+
+# def boarding_passengers(*args):
+
+#     capacity_of_ship = int(args[0])
+#     rest = args[1:]
+#     passengers_left = len(rest)
+#     passengers_on_board = {}
+#     all_on_the_board = True
+
+#     for number_of_passengers, benefit_name in rest:
+#         if capacity_of_ship == 0:
+#             break
+
+#         if capacity_of_ship >= number_of_passengers:
+#             if benefit_name not in passengers_on_board:
+#                 passengers_on_board[benefit_name] = 0
+#             passengers_on_board[benefit_name] += number_of_passengers
+#             passengers_left -= 1
+#             capacity_of_ship -= number_of_passengers
+#         else:
+#             all_on_the_board = False
+
+#     sorted_data = sorted(passengers_on_board.items(), key=lambda x: (-x[1], (x[0])))
+
+#     result = ''
+#     result += 'Boarding details by benefit plan:\n'
+
+#     for plan, count in sorted_data:
+#         result += f'## {plan}: {count} guests\n'
+
+#     if passengers_left == 0 and all_on_the_board:
+#         result += "All passengers are successfully boarded!"
+
+#     elif capacity_of_ship == 0 and passengers_left > 0:
+#         result += "Boarding unsuccessful. Cruise ship at full capacity."
+
+#     elif capacity_of_ship > 0 and not all_on_the_board:
+#         result += f"Partial boarding completed. Available capacity: {capacity_of_ship}."
+
+#     return result
