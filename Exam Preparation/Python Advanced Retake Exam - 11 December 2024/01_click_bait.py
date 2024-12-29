07086825a1fb27321ce5b9fa1fcdbf71c8781e70
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-12-29 10:50:33 +0200

    add task click_bait

---
+++ Exam Preparation/Python Advanced Retake Exam - 11 December 2024/01_click_bait.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+suggested_links = deque(map(int, input().split()))
+featured_articles = list(map(int, input().split()))
+target_value = int(input())
+final_feed = []
+
+while suggested_links and featured_articles:
+    link = suggested_links.popleft()
+    article = featured_articles.pop()
+
+    if article > link:
+        remainder = article % link
+        final_feed.append(remainder)
+
+        if remainder > 0:
+            featured_articles.append(remainder * 2)
+
+    elif link > article:
+        remainder = link % article
+        final_feed.append(-remainder)
+
+        if remainder > 0:
+            suggested_links.append(remainder * 2)
+
+    else:
+        final_feed.append(0)
+
+print(f'Final Feed: {", ".join(map(str, final_feed))}')
+total_engagement_value = sum(final_feed)
+
+if total_engagement_value >= target_value:
+    print(f'Goal achieved! Engagement Value: {total_engagement_value}')
+else:
+    short_by = target_value - total_engagement_value
+    print(f'Goal not achieved! Short by: {short_by}')
