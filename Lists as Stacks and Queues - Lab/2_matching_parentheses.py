92be3996c94947b67fc25126ebbe579d42d6b162
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-07 20:53:32 +0200

    Update 2_matching_parentheses.py

--- Lists as Stacks and Queues - Lab/2_matching_parentheses.py
+++ Lists as Stacks and Queues - Lab/2_matching_parentheses.py
@@ -1,12 +1,27 @@
-data = input()
-indexes = []
+expression = input()
 
-for i in range(len(data)):
-    ch = data[i]
+stack = []
+result = []
 
-    if ch == '(':
-        indexes.append(i)
-    elif ch == ')':
-        l = indexes.pop()
-        print(data[l:i + 1])
+for i, char in enumerate(expression):
+    if char == "(":
+        stack.append(i)
+    elif char == ")":
+        start = stack.pop()
+        result.append(expression[start:i+1])
+
+print('\n'.join(result))
+
+
+# data = input()
+# indexes = []
+
+# for i in range(len(data)):
+#     ch = data[i]
+
+#     if ch == '(':
+#         indexes.append(i)
+#     elif ch == ')':
+#         l = indexes.pop()
+#         print(data[l:i + 1])
         
