ab76a6b8921181a70803ad0eda16d9371aa0cea1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-08 21:39:00 +0200

    Update 6_balanced_paretheses_.py

--- Lists as Stacks and Queues - Exercise/6_balanced_paretheses_.py
+++ Lists as Stacks and Queues - Exercise/6_balanced_paretheses_.py
@@ -1,16 +1,35 @@
-parenthesis = input()
-
-my_stack = []
+valid_parenthesis = {'(': ')', '[': ']', '{': '}'}
+parenthesis_input = input()
 is_balanced = True
-valid_parentheses = {"(": ")", "{": "}", "[": "]"}
+my_stack = []
+
+if len(parenthesis_input) % 2 != 0:
+    is_balanced = False
+else:
+    for bracket in parenthesis_input:
+        if bracket in '({[':
+            my_stack.append(bracket)
+        else:
+            if not my_stack or bracket != valid_parenthesis[my_stack.pop()]:
+                is_balanced = False
+                break
+
+print('YES') if is_balanced else print('NO')
 
 
-for bracket in parenthesis:
-    if bracket in "({[":
-        my_stack.append(bracket)
-    else:
-        if not my_stack or bracket != valid_parentheses[my_stack.pop()]:
-            is_balanced = False
-            break
+# parenthesis = input()
 
-print("YES") if is_balanced and len(my_stack) % 2 == 0 else print("NO")
+# my_stack = []
+# is_balanced = True
+# valid_parentheses = {"(": ")", "{": "}", "[": "]"}
+
+
+# for bracket in parenthesis:
+#     if bracket in "({[":
+#         my_stack.append(bracket)
+#     else:
+#         if not my_stack or bracket != valid_parentheses[my_stack.pop()]:
+#             is_balanced = False
+#             break
+
+# print("YES") if is_balanced and len(my_stack) % 2 == 0 else print("NO")
