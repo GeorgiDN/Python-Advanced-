a42e44ad20ae62a31b742942f1e1b4ca71286f31
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-08 20:08:04 +0200

    Update 4_fashion_boutique.py

--- Lists as Stacks and Queues - Exercise/4_fashion_boutique.py
+++ Lists as Stacks and Queues - Exercise/4_fashion_boutique.py
@@ -1,20 +1,38 @@
-from collections import deque
+clothes_list = list(map(int, input().split()))
+capacity = int(input())
+racks, total_sum = 1, 0
+
+while clothes_list:
+    item = clothes_list.pop()
+    if total_sum + item <= capacity:
+        total_sum += item
+    else:
+        racks += 1
+        total_sum = 0
+        clothes_list.append(item)
+
+print(racks)
 
 
-clothes = deque(map(int, input().split(" ")))
-max_capacity = int(input())
-number_of_racks = 1
 
-total_sum_clothes = 0
-while clothes:
-    number_of_clothes = clothes.popleft()
-    total_sum_clothes += number_of_clothes
-    if total_sum_clothes > max_capacity:
-        clothes.insert(0, number_of_clothes)
-        number_of_racks += 1
-        total_sum_clothes = 0
+# from collections import deque
 
-print(number_of_racks)
+
+# clothes = deque(map(int, input().split(" ")))
+# max_capacity = int(input())
+# number_of_racks = 1
+
+# total_sum_clothes = 0
+# while clothes:
+#     number_of_clothes = clothes.popleft()
+#     total_sum_clothes += number_of_clothes
+#     if total_sum_clothes > max_capacity:
+#         clothes.insert(0, number_of_clothes)
+#         number_of_racks += 1
+#         total_sum_clothes = 0
+
+# print(number_of_racks)
+
 
 
 # from _collections import deque
