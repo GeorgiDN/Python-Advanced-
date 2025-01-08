006cf61bb72ee499be6ecc0ec12b6293e81226dd
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-08 21:01:18 +0200

    Update 5_truck_tour.py

--- Lists as Stacks and Queues - Exercise/5_truck_tour.py
+++ Lists as Stacks and Queues - Exercise/5_truck_tour.py
@@ -1,22 +1,49 @@
 from collections import deque
 
 pumps_count = int(input())
-pumps_data = deque()
+petrol_pumps = deque()
 
 for _ in range(pumps_count):
-    pumps = [int(el) for el in input().split(" ")]
-    pumps_data.append(pumps)
+    pumps_data = list(map(int, input().split()))
+    petrol_pumps.append(pumps_data)
 
 for pump in range(pumps_count):
     tank = 0
     circle_is_completed = True
-    for p in range(pumps_count):
-        tank += pumps_data[p][0] - pumps_data[p][1]
-        if tank < 0:
+    for p in petrol_pumps:
+        petrol, distance = int(p[0]), int(p[1])
+        tank += petrol
+        if tank >= distance:
+            tank -= distance
+        else:
+            petrol_pumps.append(petrol_pumps.popleft())
             circle_is_completed = False
-            pumps_data.append(pumps_data.popleft())
             break
 
     if circle_is_completed:
-        print(f"{pump}")
+        print(pump)
         break
+
+
+# from collections import deque
+
+# pumps_count = int(input())
+# pumps_data = deque()
+
+# for _ in range(pumps_count):
+#     pumps = [int(el) for el in input().split(" ")]
+#     pumps_data.append(pumps)
+
+# for pump in range(pumps_count):
+#     tank = 0
+#     circle_is_completed = True
+#     for p in range(pumps_count):
+#         tank += pumps_data[p][0] - pumps_data[p][1]
+#         if tank < 0:
+#             circle_is_completed = False
+#             pumps_data.append(pumps_data.popleft())
+#             break
+
+#     if circle_is_completed:
+#         print(f"{pump}")
+#         break
