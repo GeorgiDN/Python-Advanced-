85ff9c00c6d910ceb804567ad11f31f0bc41a709
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-08 19:47:54 +0200

    Update 3_fast_food.py

--- Lists as Stacks and Queues - Exercise/3_fast_food.py
+++ Lists as Stacks and Queues - Exercise/3_fast_food.py
@@ -1,18 +1,37 @@
 from collections import deque
 
-total_sum = int(input())
-orders = deque(map(int, input().split(" ")))
-print(max(orders))
+food = int(input())
+orders = deque(map(int, input().split()))
+max_order = max(orders)
 
-for _ in range(len(orders)):
-    current_order = orders.popleft()
-    if current_order <= total_sum:
-        total_sum -= current_order
+while orders:
+    order = orders.popleft()
+    if order <= food:
+        food -= order
     else:
-        orders.insert(0, current_order)
+        orders.appendleft(order)
+        break
 
-if orders:
-    print(f"Orders left: {' '.join(map(str, orders))}")
-else:
-    print("Orders complete")
+print(max_order)
+print('Orders complete') if not orders else print(f'Orders left: {" ".join(map(str, orders))}')
+
+
+
+# from collections import deque
+
+# total_sum = int(input())
+# orders = deque(map(int, input().split(" ")))
+# print(max(orders))
+
+# for _ in range(len(orders)):
+#     current_order = orders.popleft()
+#     if current_order <= total_sum:
+#         total_sum -= current_order
+#     else:
+#         orders.insert(0, current_order)
+
+# if orders:
+#     print(f"Orders left: {' '.join(map(str, orders))}")
+# else:
+#     print("Orders complete")
 
