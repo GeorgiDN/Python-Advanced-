ec530736234afdef400ac412153c53b4f7882a35
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-12 16:01:30 +0200

    Update 2_even_matrix.py

--- Multidimensional Lists - Lab/2_even_matrix.py
+++ Multidimensional Lists - Lab/2_even_matrix.py
@@ -1,6 +1,13 @@
 rows = int(input())
-matrix = [[int(x) for x in input().split(", ") if int(x) % 2 == 0] for _ in range(rows)]
-print(matrix)
+matrix = [list(map(int, input().split(', '))) for _ in range(rows)]
+even_matrix = [[num for num in row if num % 2 == 0] for row in matrix]
+print(even_matrix)
+
+
+##########################################################################################
+# rows = int(input())
+# matrix = [[int(x) for x in input().split(", ") if int(x) % 2 == 0] for _ in range(rows)]
+# print(matrix)
 
 
 ##########################################################################################
@@ -16,20 +23,20 @@ print(matrix)
 # def read_matrix(r):
 #     matrix = [[int(x) for x in input().split(", ")] for _ in range(r)]
 #     return matrix
-# 
-# 
+#
+#
 # def get_even_row(row):
 #     result = [x for x in row if x % 2 == 0]
 #     return result
-# 
-# 
+#
+#
 # def get_even_matrix(matrix):
 #     result = [get_even_row(row) for row in matrix]
 #     return result
-# 
-# 
+#
+#
 # rows = int(input())
-# 
+#
 # print(get_even_matrix(read_matrix(rows)))
 
 
