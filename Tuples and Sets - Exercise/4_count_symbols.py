1db7c2a7dd7fe5d25a60a0c415c1e24272df3508
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-12 14:39:31 +0200

    Update 4_count_symbols.py

--- Tuples and Sets - Exercise/4_count_symbols.py
+++ Tuples and Sets - Exercise/4_count_symbols.py
@@ -1,6 +1,27 @@
 from collections import Counter
 
 text = input()
-occurrences = Counter(text)
-sorted_occurrences = dict(sorted(occurrences.items()))
-print("\n".join([f"{char}: {count} time/s" for char, count in sorted_occurrences.items()]))
+[print(f'{letter}: {count} time/s') for letter, count in sorted(Counter(text).items())]
+
+
+# from collections import Counter
+# [print(f'{letter}: {count} time/s') for letter, count in sorted(Counter(input()).items())]
+
+
+
+# from collections import defaultdict
+#
+# characters_data = defaultdict(int)
+# text = input()
+# for char in text:
+#     characters_data[char] += 1
+# [print(f'{letter}: {count} time/s') for letter, count in sorted(characters_data.items())]
+
+
+
+# from collections import Counter
+
+# text = input()
+# occurrences = Counter(text)
+# sorted_occurrences = dict(sorted(occurrences.items()))
+# print("\n".join([f"{char}: {count} time/s" for char, count in sorted_occurrences.items()]))
