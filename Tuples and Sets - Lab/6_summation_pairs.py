9c3e0f85955f851e1e74b59eed4c1eb114411fed
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-12 14:07:45 +0200

    Update 6_summation_pairs.py

--- Tuples and Sets - Lab/6_summation_pairs.py
+++ Tuples and Sets - Lab/6_summation_pairs.py
@@ -1,26 +1,49 @@
-list_numbers = [int(x) for x in input().split()]
+from collections import deque
+
+
+nums = deque(map(int, input().split()))
 target = int(input())
-matches = []
-used_numbers = []
-
 idx = 0
-while True:
-    if idx + 1 > len(list_numbers):
-        break
+pairs = []
 
-    num1 = list_numbers[idx]
-
-    for num2 in list_numbers:
-        if num2 != num1:
-            if num2 not in used_numbers and num1 not in used_numbers:
-                if num1 + num2 == target:
-                    matches.append([num1, num2])
-                    used_numbers.append(num2)
+while idx < len(nums):
+    num1 = nums.popleft()
+    for num2 in nums:
+        if num1 + num2 == target:
+            if (num1, num2) not in pairs and (num2, num1) not in pairs:
+                pairs.append((num1, num2))
+    nums.append(num1)
     idx += 1
 
-for data in matches:
-    n1, n2 = data[0], data[1]
-    print(f"{n1} + {n2} = {target}")
+for pair in pairs:
+    print(f'{pair[0]} + {pair[1]} = {target}')
+
+
+
+
+# list_numbers = [int(x) for x in input().split()]
+# target = int(input())
+# matches = []
+# used_numbers = []
+
+# idx = 0
+# while True:
+#     if idx + 1 > len(list_numbers):
+#         break
+
+#     num1 = list_numbers[idx]
+
+#     for num2 in list_numbers:
+#         if num2 != num1:
+#             if num2 not in used_numbers and num1 not in used_numbers:
+#                 if num1 + num2 == target:
+#                     matches.append([num1, num2])
+#                     used_numbers.append(num2)
+#     idx += 1
+
+# for data in matches:
+#     n1, n2 = data[0], data[1]
+#     print(f"{n1} + {n2} = {target}")
 
 
 
