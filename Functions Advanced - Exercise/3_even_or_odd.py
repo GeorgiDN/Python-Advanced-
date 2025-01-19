33552049966d243b660982466b9868113016dbd5
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-19 20:09:30 +0200

    Update 3_even_or_odd.py

--- Functions Advanced - Exercise/3_even_or_odd.py
+++ Functions Advanced - Exercise/3_even_or_odd.py
@@ -1,16 +1,22 @@
 def even_odd(*args):
-    nums, command = list(map(int, args[:-1])), args[-1]
-    if command == "even":
-        return [n for n in nums if n % 2 == 0]
-    return [n for n in nums if n % 2 != 0]
+    nums, kind = args[:-1], args[-1]
+    return [x for x in nums if x % 2 == 0] if kind == 'even' else [x for x in nums if x % 2 != 0]
 
 
-print(even_odd(1, 2, 3, 4, 5, 6, "even"))
-print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
+#########################################################################################################################
+# def even_odd(*args):
+#     nums, command = list(map(int, args[:-1])), args[-1]
+#     if command == "even":
+#         return [n for n in nums if n % 2 == 0]
+#     return [n for n in nums if n % 2 != 0]
+
+
+# print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+# print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
 
 
 
-
+#########################################################################################################################
 # def even_odd(*args):
 #     *numbers, command = args
 #     result = []
