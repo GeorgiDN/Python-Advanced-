cdc0a46c26d44f76968ed6e86b9ecd6315aaad75
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-25 21:54:15 +0200

    Update 2_email_validator.py

--- Error Handling-Exercise/2_email_validator.py
+++ Error Handling-Exercise/2_email_validator.py
@@ -10,25 +10,78 @@ class InvalidDomainError(Exception):
     pass
 
 
-while True:
-    email = input()
+VALID_DOMAINS = ("com", "bg", "org", "net")
+MIN_LENGTH_NAME = 4
 
-    if email == "End":
-        break
 
-    name = email.split("@")[0]
-    domain = email.split(".")[-1]
+def check_for_email_symbol(email, symbol="@"):
+    if email.count(symbol) != 1:
+        raise MustContainAtSymbolError(f"Email must contain {symbol}")
 
-    if len(name) <= 4:
-        raise NameTooShortError("Name must be more than 4 characters")
 
-    if email.count("@") != 1:
-        raise MustContainAtSymbolError("Email must contain @")
+def check_name_length(name):
+    if len(name) <= MIN_LENGTH_NAME:
+        raise NameTooShortError(f"Name must be more than 4 characters")
 
-    if domain not in ["com", "bg", "org", "net"]:
+
+def check_is_valid_domain(domain):
+    if domain not in VALID_DOMAINS:
         raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
 
-    print("Email is valid")
+
+def validate_email():
+    while True:
+        email = input()
+        if email == "End":
+            break
+
+        check_for_email_symbol(email)
+
+        name = email.split("@")[0]
+        check_name_length(name)
+
+        domain = email.split(".")[-1]
+        check_is_valid_domain(domain)
+
+        print("Email is valid")
+
+
+validate_email()
+
+
+
+####################################################################################
+# class NameTooShortError(Exception):
+#     pass
+
+
+# class MustContainAtSymbolError(Exception):
+#     pass
+
+
+# class InvalidDomainError(Exception):
+#     pass
+
+
+# while True:
+#     email = input()
+
+#     if email == "End":
+#         break
+
+#     name = email.split("@")[0]
+#     domain = email.split(".")[-1]
+
+#     if len(name) <= 4:
+#         raise NameTooShortError("Name must be more than 4 characters")
+
+#     if email.count("@") != 1:
+#         raise MustContainAtSymbolError("Email must contain @")
+
+#     if domain not in ["com", "bg", "org", "net"]:
+#         raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+
+#     print("Email is valid")
 
 
 ######################################################################################  CONDITION  ##########################################################################################################################################
