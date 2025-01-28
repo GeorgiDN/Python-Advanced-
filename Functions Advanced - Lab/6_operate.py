0fe2a2b677bc5c59cf523e4687d0f32997611181
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-01-28 19:48:21 +0200

    Update 6_operate.py

--- Functions Advanced - Lab/6_operate.py
+++ Functions Advanced - Lab/6_operate.py
@@ -1,26 +1,39 @@
 from functools import reduce
+import operator
 
 
-def operate(operator, *args):
-    if operator == '+':
-        return reduce(lambda x, y: x + y, args)
-    elif operator == '-':
-        return reduce(lambda x, y: x - y, args)
-    elif operator == '*':
-        return reduce(lambda x, y: x * y, args)
-    elif operator == '/':
-        if 0 in args:
-            return 0
-        return reduce(lambda x, y: x / y, args)
+def operate(operator_symbol, *args):
+    operations = {
+        "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv,
+    }
+
+    return reduce(operations[operator_symbol], args)
 
 
 print(operate("+", 1, 2, 3))
-print(operate("/", 0, 0, 3))
-
-print(operate("/", 2, 2, 3))
-print(operate("*", 3, 4))
 
 
+##################################################################################################
+# from functools import reduce
+# 
+# def operate(operator, *args):
+#     if operator == '+':
+#         return reduce(lambda x, y: x + y, args)
+#     elif operator == '-':
+#         return reduce(lambda x, y: x - y, args)
+#     elif operator == '*':
+#         return reduce(lambda x, y: x * y, args)
+#     elif operator == '/':
+#         if 0 in args:
+#             return 0
+#         return reduce(lambda x, y: x / y, args)
+
+
+
+####################################################################################################
 # print(operate("/", 2, 2, 3))
 # print(operate("*", 3, 4))
 
