b4f5c2573ddf06ca6aa751e36a8207470c43cb6c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-02 21:14:23 +0200

    Update 7_selection_sort.py

--- Algorithms Introduction - Lab/7_selection_sort.py
+++ Algorithms Introduction - Lab/7_selection_sort.py
@@ -1,14 +1,28 @@
-def selection_sor(nums):
-    for idx in range(len(nums)):
-        min_idx = idx
+def selection_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
 
-        for curr_idx in range(idx + 1, len(nums)):
-            if nums[curr_idx] < nums[min_idx]:
-                min_idx = curr_idx
-
-        nums[idx], nums[min_idx] = nums[min_idx], nums[idx]
+    return ' '.join(map(str, arr))
 
 
-nums = list(map(int, input().split()))
-selection_sor(nums)
-print(*nums)
+arr = list(map(int, input().split()))
+print(selection_sort(arr))
+
+
+#############################################################
+# def selection_sor(nums):
+#     for idx in range(len(nums)):
+#         min_idx = idx
+
+#         for curr_idx in range(idx + 1, len(nums)):
+#             if nums[curr_idx] < nums[min_idx]:
+#                 min_idx = curr_idx
+
+#         nums[idx], nums[min_idx] = nums[min_idx], nums[idx]
+
+
+# nums = list(map(int, input().split()))
+# selection_sor(nums)
+# print(*nums)
