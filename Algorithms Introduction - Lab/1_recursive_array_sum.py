fccd0a0cfdb2c68c47eee88cfaa548ca33dbc739
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-02 17:21:07 +0200

    Update 1_recursive_array_sum.py

--- Algorithms Introduction - Lab/1_recursive_array_sum.py
+++ Algorithms Introduction - Lab/1_recursive_array_sum.py
@@ -6,3 +6,17 @@ def calculate_sum(nums, index):
 
 list_numbers = [int(x) for x in input().split()]
 print(calculate_sum(list_numbers, 0))
+
+
+
+#####################################################
+# def calculate_sum(array):
+#     if len(array) == 1:
+#         return array[0]
+
+#     return array[0] + calculate_sum(array[1:])
+
+
+# list_numbers = [int(x) for x in input().split()]
+# print(calculate_sum(list_numbers))
+
