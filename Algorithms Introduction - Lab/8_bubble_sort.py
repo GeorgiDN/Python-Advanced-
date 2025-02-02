49a69c0270339f77bae355a5ab735d0387633a93
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-02 21:50:46 +0200

    Update 8_bubble_sort.py

--- Algorithms Introduction - Lab/8_bubble_sort.py
+++ Algorithms Introduction - Lab/8_bubble_sort.py
@@ -1,66 +1,18 @@
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+def bubble_sort(nums):
+    is_sorted = False
+    i = 0
 
-    return ' '.join(map(str, arr))
+    while not is_sorted:
+        is_sorted = True
 
+        for j in range(1, len(nums) - i):
+            if nums[j - 1] > nums[j]:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
+                is_sorted = False
 
-arr = list(map(int, input().split()))
-print(bubble_sort(arr))
-
-
-######################################################
-# def bubble_sort(nums):
-#     is_sorted = False
-#     i = 0
-
-#     while not is_sorted:
-#         is_sorted = True
-
-#         for j in range(1, len(nums) - i):
-#             if nums[j - 1] > nums[j]:
-#                 nums[j], nums[j - 1] = nums[j - 1], nums[j]
-#                 is_sorted = False
-
-#         i += 1
+        i += 1
 
 
-# nums = list(map(int, input().split()))
-# bubble_sort(nums)
-# print(*nums)
-
-
-
-
-# # MEASURE TIME
-# import time
-#
-
-# def bubble_sort(nums):
-#     is_sorted = False
-#     i = 0
-#
-#     while not is_sorted:
-#         is_sorted = True
-#
-#         for j in range(1, len(nums) - i):
-#             if nums[j - 1] > nums[j]:
-#                 nums[j], nums[j - 1] = nums[j - 1], nums[j]
-#                 is_sorted = False
-#
-#         i += 1
-#
-#
-# nums = list(map(int, input().split()))
-#
-# start_time = time.time()
-#
-# bubble_sort(nums)
-# print(*nums)
-#
-# # Calculate and print execution time
-# end_time = time.time()
-# execution_time = end_time - start_time
-# print("Execution time:", execution_time, "seconds")
+nums = list(map(int, input().split()))
+bubble_sort(nums)
+print(*nums)
