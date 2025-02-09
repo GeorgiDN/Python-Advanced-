ec24b1d55c1bbabd57b5d0b51bd443760801bb0b
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-09 10:47:45 +0200

    Update 3_cook_book.py

--- Exam Preparation/Python Advanced Exam - 17 February 2024/3_cook_book.py
+++ Exam Preparation/Python Advanced Exam - 17 February 2024/3_cook_book.py
@@ -1,27 +1,53 @@
 def cookbook(*args):
-    ingredients_book = {}
+    recipies = {}
+    all_ingredients = {}
 
-    for elements in args:
-        cuisine = elements[1]
-        recipe_name = elements[0]
-        ingredients = elements[2]
+    for cook in args:
+        cuisine, food, ingredients = cook[1], cook[0], cook[2]
 
-        if cuisine not in ingredients_book:
-            ingredients_book[cuisine] = {}
+        if cuisine not in recipies:
+            recipies[cuisine] = []
+        recipies[cuisine].append(food)
+        all_ingredients[food] = ingredients
 
-        if recipe_name not in ingredients_book[cuisine]:
-            ingredients_book[cuisine][recipe_name] = ingredients
+    result = []
 
-    result = ''
-    sorted_ingredients_book = dict(sorted(ingredients_book.items(), key=lambda x: (-len(x[1]), (x[0]))))
+    sorted_recipes = dict(sorted(recipies.items(), key=lambda x: (-len(x[1]), x[0])))
 
-    for cuisine, recipies in sorted_ingredients_book.items():
-        recipies = dict(sorted(recipies.items()))
-        result += f"{cuisine} cuisine contains {len(recipies)} recipes:\n"
-        for recipie, ingredients in recipies.items():
-            result += f"  * {recipie} -> Ingredients: {', '.join(ingredients)}\n"
+    for cuisine, foods in sorted_recipes.items():
+        result.append(f'{cuisine} cuisine contains {len(foods)} recipes:')
+        foods.sort()
+        for food in foods:
+            result.append(f'  * {food} -> Ingredients: {", ".join(all_ingredients[food])}')
 
-    return result
+    return '\n'.join(result)
+
+
+############################################################################################################################################################
+# def cookbook(*args):
+#     ingredients_book = {}
+
+#     for elements in args:
+#         cuisine = elements[1]
+#         recipe_name = elements[0]
+#         ingredients = elements[2]
+
+#         if cuisine not in ingredients_book:
+#             ingredients_book[cuisine] = {}
+
+#         if recipe_name not in ingredients_book[cuisine]:
+#             ingredients_book[cuisine][recipe_name] = ingredients
+
+#     result = ''
+#     sorted_ingredients_book = dict(sorted(ingredients_book.items(), key=lambda x: (-len(x[1]), (x[0]))))
+
+#     for cuisine, recipies in sorted_ingredients_book.items():
+#         recipies = dict(sorted(recipies.items()))
+#         result += f"{cuisine} cuisine contains {len(recipies)} recipes:\n"
+#         for recipie, ingredients in recipies.items():
+#             result += f"  * {recipie} -> Ingredients: {', '.join(ingredients)}\n"
+
+#     return result
 
 
 # print(cookbook(
