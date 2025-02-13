8901a1cc327452adfeb45fa8456e8206773a22a1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-13 09:35:44 +0200

    Update 01_click_bait.py

--- Exam Preparation/Python Advanced Retake Exam - 11 December 2024/01_click_bait.py
+++ Exam Preparation/Python Advanced Retake Exam - 11 December 2024/01_click_bait.py
@@ -1,7 +1,7 @@
 from collections import deque
 
-suggested_links = deque(map(int, input().split()))  # FIFO
-featured_articles = list(map(int, input().split()))  # LIFO
+suggested_links = deque(map(int, input().split()))
+featured_articles = list(map(int, input().split()))
 target_value = int(input())
 final_feed = []
 
@@ -12,64 +12,63 @@ while suggested_links and featured_articles:
     if article > link:
         remainder = article % link
         final_feed.append(remainder)
-        remainder *= 2
-        if remainder != 0:
-            featured_articles.append(remainder)
 
-    elif article < link:
+        if remainder > 0:
+            featured_articles.append(remainder * 2)
+
+    elif link > article:
         remainder = link % article
         final_feed.append(-remainder)
-        remainder *= 2
-        if remainder != 0:
-            suggested_links.append(remainder)
+
+        if remainder > 0:
+            suggested_links.append(remainder * 2)
 
     else:
         final_feed.append(0)
 
+print(f'Final Feed: {", ".join(map(str, final_feed))}')
 total_engagement_value = sum(final_feed)
-print(f"Final Feed: {', '.join(map(str, final_feed))}")
+
 if total_engagement_value >= target_value:
-    print(f"Goal achieved! Engagement Value: {total_engagement_value}")
+    print(f'Goal achieved! Engagement Value: {total_engagement_value}')
 else:
-    shortfall = target_value - total_engagement_value
-    print(f"Goal not achieved! Short by: {shortfall}")
+    short_by = target_value - total_engagement_value
+    print(f'Goal not achieved! Short by: {short_by}')
 
 
-
-##############################################################################################
+############################################################
 # from collections import deque
-
+#
 # suggested_links = deque(map(int, input().split()))
-# featured_articles = list(map(int, input().split()))
-# target_value = int(input())
-# final_feed = []
-
+# featured_articles = list(map(int, input().split())) # LIFO
+# final_collection = []
+# target_rate = int(input())
+#
 # while suggested_links and featured_articles:
 #     link = suggested_links.popleft()
 #     article = featured_articles.pop()
-
+#
 #     if article > link:
 #         remainder = article % link
-#         final_feed.append(remainder)
-
+#         final_collection.append(remainder)
+#
 #         if remainder > 0:
 #             featured_articles.append(remainder * 2)
-
+#
 #     elif link > article:
 #         remainder = link % article
-#         final_feed.append(-remainder)
-
+#         final_collection.append(-remainder)
+#
 #         if remainder > 0:
 #             suggested_links.append(remainder * 2)
-
+#
 #     else:
-#         final_feed.append(0)
-
-# print(f'Final Feed: {", ".join(map(str, final_feed))}')
-# total_engagement_value = sum(final_feed)
-
-# if total_engagement_value >= target_value:
+#         final_collection.append(0)
+#
+# total_engagement_value = sum(final_collection)
+# print(f'Final Feed: {", ".join(map(str, final_collection))}')
+#
+# if total_engagement_value >= target_rate:
 #     print(f'Goal achieved! Engagement Value: {total_engagement_value}')
 # else:
-#     short_by = target_value - total_engagement_value
-#     print(f'Goal not achieved! Short by: {short_by}')
+#     print(f'Goal not achieved! Short by: {target_rate - total_engagement_value}')
