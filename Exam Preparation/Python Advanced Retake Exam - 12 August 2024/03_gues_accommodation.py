978989e6fa178bcd822a6cb5e7ea260f5f8f4f89
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-03-19 08:55:42 +0200

    Update 03_gues_accommodation.py

--- Exam Preparation/Python Advanced Retake Exam - 12 August 2024/03_gues_accommodation.py
+++ Exam Preparation/Python Advanced Retake Exam - 12 August 2024/03_gues_accommodation.py
@@ -32,26 +32,65 @@ def accommodate(*args, **kwargs):
 
     return '\n'.join(result)
 
-# print(accommodate(5, 4, 2, room_305=6, room_410=5, room_204=2))
-# print(accommodate(10, 9, 8, room_307=6, room_802=5))
-# print(accommodate(1, 2, 4, 8, room_102=3, room_101=1, room_103=2))
+
+
+#################################################################################
+
+# def accommodate(*args, **kwargs):
+#     rooms = {}
+#     for key, value in kwargs.items():
+#         key = int(key.split('_')[1])
+#         rooms[key] = value
+# 
+#     sorted_rooms = dict(sorted(rooms.items(), key=lambda r: (r[1], r[0])))
+# 
+#     accommodated = {}
+# 
+#     for guests in args:
+#         for room_number, capacity in sorted_rooms.items():
+#             if room_number not in accommodated and guests <= capacity:
+#                 accommodated[room_number] = guests
+#                 break
+# 
+#     all_accommodated = len(args) == len(accommodated)
+# 
+#     result = []
+# 
+#     if accommodated:
+#         sorted_accommodated = dict(sorted(accommodated.items(), key=lambda r: r[0]))
+#         result.append(f"A total of {len(accommodated)} accommodations were completed!")
+# 
+#         for room, num_guests in sorted_accommodated.items():
+#             result.append(f"<Room {room} accommodates {num_guests} guests>")
+# 
+#     else:
+#         result.append("No accommodations were completed!")
+# 
+#     if not all_accommodated:
+#         guest_with_no_accommodation = sum(args) - sum(accommodated.values())
+#         result.append(f"Guests with no accommodation: {guest_with_no_accommodation}")
+# 
+#     empty_rooms = len(kwargs) - len(accommodated)
+#     if empty_rooms:
+#         result.append(f"Empty rooms: {empty_rooms}")
+# 
+#     return "\n".join(result)
 
 
 
-
-########################################################################################################################
+#################################################################################
 # def accommodate(*args, **kwargs):
 #     groups = list(args)
 #     rooms = {}
 #     accommodated_guest = {}
-
+#
 #     for key, value in kwargs.items():
 #         key = int(key.split("_")[1])
 #         rooms[key] = value
-
+#
 #     sorted_rooms = sorted(rooms.items(), key=lambda x: (x[1], x[0]))
 #     not_accomodated = []
-
+#
 #     for guests in groups:
 #         for room in sorted_rooms:
 #             room_number, capacity = room[0], room[1]
@@ -61,10 +100,10 @@ def accommodate(*args, **kwargs):
 #                     break
 #         else:
 #             not_accomodated.append(guests)
-
+#
 #     empty_rooms = len(kwargs) - len(accommodated_guest)
 #     total_number_of_unaccommodated_guests = sum(not_accomodated)
-
+#
 #     result = []
 #     if accommodated_guest:
 #         result.append(f"A total of {len(accommodated_guest)} accommodations were completed!")
@@ -73,12 +112,52 @@ def accommodate(*args, **kwargs):
 #             result.append(f"<Room {r} accommodates {g} guests>")
 #     else:
 #         result.append("No accommodations were completed!")
-
+#
 #     if not_accomodated:
 #         result.append(f"Guests with no accommodation: {total_number_of_unaccommodated_guests}")
-
+#
 #     if empty_rooms > 0:
 #         result.append(f"Empty rooms: {empty_rooms}")
-
+#
 #     return "\n".join(result)
-    
+
+
+
+#################################################################################
+# def accommodate(*args, **kwargs):
+#     guests = list(args)
+#     rooms = list(kwargs.items())
+#
+#     rooms.sort(key=lambda x: (x[1], int(x[0].split('_')[1])))
+#
+#     accommodations = []
+#     unaccommodated_guests = 0
+#     for group in guests:
+#         accommodated = False
+#         for i, (room, capacity) in enumerate(rooms):
+#             if group <= capacity:
+#                 accommodations.append((int(room.split('_')[1]), group))
+#                 rooms.pop(i)
+#                 accommodated = True
+#                 break
+#         if not accommodated:
+#             unaccommodated_guests += group
+#
+#     if accommodations:
+#         accommodations.sort()
+#         result = [f"A total of {len(accommodations)} accommodations were completed!"]
+#         for room_number, guests in accommodations:
+#             result.append(f"<Room {room_number} accommodates {guests} guests>")
+#         result = "\n".join(result)
+#     else:
+#         result = "No accommodations were completed!"
+#
+#     if unaccommodated_guests > 0:
+#         result += f"\nGuests with no accommodation: {unaccommodated_guests}"
+#
+#     if rooms:
+#         result += f"\nEmpty rooms: {len(rooms)}"
+#
+#     return result
+# #
+#
